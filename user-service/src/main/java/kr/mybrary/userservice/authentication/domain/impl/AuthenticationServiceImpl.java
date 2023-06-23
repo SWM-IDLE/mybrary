fa1cddd82709e55183924c6405c0369b41b9d8d9
0112leesy <0112leesy@naver.com>
2023-06-23 16:49:38 +0900

    [IDLE-61] refactor#82: AuthenticationService 추상화
    	- DIP를 고려해 AuthenticationService를 인터페이스와 구현체로 분리
    	- 코드 리뷰 반영 (p3: application 계층에서 presentation 계층으로 데이터를 전달할 때 DTO로 변환해서 반환)
    	- AuthenticationService 관련 테스트 코드 수정

---
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/domain/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,46 @@
+package kr.mybrary.userservice.authentication.domain.impl;
+
+import jakarta.transaction.Transactional;
+import kr.mybrary.userservice.authentication.domain.AuthenticationService;
+import kr.mybrary.userservice.authentication.domain.dto.SignUpResponse;
+import kr.mybrary.userservice.authentication.domain.dto.UserMapper;
+import kr.mybrary.userservice.authentication.domain.exception.DuplicateUserInfoException;
+import kr.mybrary.userservice.user.persistence.Role;
+import kr.mybrary.userservice.user.persistence.User;
+import kr.mybrary.userservice.authentication.presentation.dto.request.SignUpRequest;
+import kr.mybrary.userservice.user.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SignUpResponse signUp(SignUpRequest signUpRequest) {
+
+        if (userRepository.findByLoginId(signUpRequest.getLoginId()).isPresent()) {
+            throw new DuplicateUserInfoException();
+        }
+        if (userRepository.findByNickname(signUpRequest.getNickname()).isPresent()) {
+            throw new DuplicateUserInfoException();
+        }
+        if (userRepository.findByEmail(signUpRequest.getEmail()).isPresent()) {
+            throw new DuplicateUserInfoException();
+        }
+
+        User user = UserMapper.INSTANCE.toEntity(signUpRequest);
+        user.updatePassword(passwordEncoder.encode(user.getPassword()));
+        user.updateRole(Role.USER);
+        SignUpResponse signUpResponse = UserMapper.INSTANCE.toResponse(userRepository.save(user));
+
+        return signUpResponse;
+
+    }
+
+
+}
--- user-service/src/test/java/kr/mybrary/userservice/authentication/domain/impl/AuthenticationServiceImplTest.java
+++ user-service/src/test/java/kr/mybrary/userservice/authentication/domain/impl/AuthenticationServiceImplTest.java
@@ -1,4 +1,4 @@
-package kr.mybrary.userservice.authentication.domain;
+package kr.mybrary.userservice.authentication.domain.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -8,6 +8,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
+import kr.mybrary.userservice.authentication.domain.dto.SignUpResponse;
+import kr.mybrary.userservice.authentication.domain.dto.UserMapper;
 import kr.mybrary.userservice.authentication.domain.exception.DuplicateUserInfoException;
 import kr.mybrary.userservice.authentication.presentation.dto.request.SignUpRequest;
 import kr.mybrary.userservice.user.persistence.Role;
@@ -23,7 +25,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
-class AuthenticationServiceTest {
+class AuthenticationServiceImplTest {
 
     @Mock
     UserRepository userRepository;
@@ -31,41 +33,50 @@ class AuthenticationServiceTest {
     PasswordEncoder passwordEncoder;
 
     @InjectMocks
-    AuthenticationService authenticationService;
+    AuthenticationServiceImpl authenticationService;
 
     @Test
     @DisplayName("회원가입 요청이 들어오면 암호화된 비밀번호와 함께 회원 권한으로 사용자 정보를 저장한다")
     void signUp() {
         // Given
         SignUpRequest signUpRequest = SignUpRequest.builder()
-            .loginId("loginId")
-            .password("password123!")
-            .nickname("nickname")
-            .email("email@mail.com")
-            .build();
+                .loginId("loginId")
+                .password("password123!")
+                .nickname("nickname")
+                .email("email@mail.com")
+                .build();
 
         given(userRepository.findByLoginId(signUpRequest.getLoginId())).willReturn(Optional.empty());
         given(userRepository.findByNickname(signUpRequest.getNickname())).willReturn(Optional.empty());
         given(userRepository.findByEmail(signUpRequest.getEmail())).willReturn(Optional.empty());
         given(passwordEncoder.encode(signUpRequest.getPassword())).willReturn("encodedPassword");
-        given(userRepository.save(any(User.class))).will(returnsFirstArg());
+        given(userRepository.save(any(User.class))).willReturn(
+                User.builder()
+                        .loginId(signUpRequest.getLoginId())
+                        .password("encodedPassword")
+                        .nickname(signUpRequest.getNickname())
+                        .email(signUpRequest.getEmail())
+                        .role(Role.USER)
+                        .build()
+        );
 
         // When
-        User savedUser = authenticationService.signUp(signUpRequest);
+        SignUpResponse savedUser = authenticationService.signUp(signUpRequest);
 
         // Then
         Assertions.assertAll(
-            () -> assertThat(savedUser.getLoginId()).isEqualTo(signUpRequest.getLoginId()),
-            () -> assertThat(savedUser.getNickname()).isEqualTo(signUpRequest.getNickname()),
-            () -> assertThat(savedUser.getEmail()).isEqualTo(signUpRequest.getEmail()),
-            () -> assertThat(savedUser.getPassword()).isEqualTo("encodedPassword"),
-            () -> assertThat(savedUser.getRole()).isEqualTo(Role.USER)
+                () -> assertThat(savedUser.getLoginId()).isEqualTo(signUpRequest.getLoginId()),
+                () -> assertThat(savedUser.getNickname()).isEqualTo(signUpRequest.getNickname()),
+                () -> assertThat(savedUser.getEmail()).isEqualTo(signUpRequest.getEmail()),
+                () -> assertThat(savedUser.getPassword()).isEqualTo("encodedPassword"),
+                () -> assertThat(savedUser.getRole()).isEqualTo(Role.USER)
         );
 
         verify(userRepository).findByLoginId(signUpRequest.getLoginId());
         verify(userRepository).findByEmail(signUpRequest.getEmail());
         verify(userRepository).findByNickname(signUpRequest.getNickname());
         verify(userRepository).save(any(User.class));
+
     }
 
     @Test
@@ -73,24 +84,24 @@ class AuthenticationServiceTest {
     void DuplicateLoginId() {
         // Given
         SignUpRequest signUpRequest = SignUpRequest.builder()
-            .loginId("loginId")
-            .password("password123!")
-            .nickname("nickname")
-            .email("email@mail.com")
-            .build();
+                .loginId("loginId")
+                .password("password123!")
+                .nickname("nickname")
+                .email("email@mail.com")
+                .build();
 
         User user = User.builder()
-            .loginId("loginId")
-            .build();
+                .loginId("loginId")
+                .build();
 
         given(userRepository.findByLoginId(signUpRequest.getLoginId())).willReturn(
-            Optional.of(user));
+                Optional.of(user));
 
         // When
         assertThatThrownBy(() -> authenticationService.signUp(signUpRequest))
-            .isInstanceOf(DuplicateUserInfoException.class)
-            .hasFieldOrPropertyWithValue("errorCode", "U-01")
-            .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
+                .isInstanceOf(DuplicateUserInfoException.class)
+                .hasFieldOrPropertyWithValue("errorCode", "U-01")
+                .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
 
         // Then
         verify(userRepository).findByLoginId(signUpRequest.getLoginId());
@@ -101,24 +112,24 @@ class AuthenticationServiceTest {
     void DuplicateNickname() {
         // Given
         SignUpRequest signUpRequest = SignUpRequest.builder()
-            .loginId("loginId")
-            .password("password123!")
-            .nickname("nickname")
-            .email("email@mail.com")
-            .build();
+                .loginId("loginId")
+                .password("password123!")
+                .nickname("nickname")
+                .email("email@mail.com")
+                .build();
 
         User user = User.builder()
-            .nickname("nickname")
-            .build();
+                .nickname("nickname")
+                .build();
 
         given(userRepository.findByNickname(signUpRequest.getNickname())).willReturn(
-            Optional.of(user));
+                Optional.of(user));
 
         // When
         assertThatThrownBy(() -> authenticationService.signUp(signUpRequest))
-            .isInstanceOf(DuplicateUserInfoException.class)
-            .hasFieldOrPropertyWithValue("errorCode", "U-01")
-            .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
+                .isInstanceOf(DuplicateUserInfoException.class)
+                .hasFieldOrPropertyWithValue("errorCode", "U-01")
+                .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
 
         // Then
         verify(userRepository).findByNickname(signUpRequest.getNickname());
@@ -129,30 +140,27 @@ class AuthenticationServiceTest {
     void DuplicateEmail() {
         // Given
         SignUpRequest signUpRequest = SignUpRequest.builder()
-            .loginId("loginId")
-            .password("password123!")
-            .nickname("nickname")
-            .email("email@mail.com")
-            .build();
+                .loginId("loginId")
+                .password("password123!")
+                .nickname("nickname")
+                .email("email@mail.com")
+                .build();
 
         User user = User.builder()
-            .email("email@mail.com")
-            .build();
+                .email("email@mail.com")
+                .build();
 
         given(userRepository.findByEmail(signUpRequest.getEmail())).willReturn(
-            Optional.of(user));
+                Optional.of(user));
 
         // When
         assertThatThrownBy(() -> authenticationService.signUp(signUpRequest))
-            .isInstanceOf(DuplicateUserInfoException.class)
-            .hasFieldOrPropertyWithValue("errorCode", "U-01")
-            .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
+                .isInstanceOf(DuplicateUserInfoException.class)
+                .hasFieldOrPropertyWithValue("errorCode", "U-01")
+                .hasFieldOrPropertyWithValue("errorMessage", "이미 존재하는 회원입니다.");
 
         // Then
         verify(userRepository).findByEmail(signUpRequest.getEmail());
     }
-
-
-
 
 }
