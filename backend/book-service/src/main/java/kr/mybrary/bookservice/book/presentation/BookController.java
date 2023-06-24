bbc3b564ae969f5b4babc59be6bc09e4eb4189cd
minseong <kdsvip5@naver.com>
2023-06-25 01:57:46 +0900

    [IDLE-83] feat#88: BookController 추가 - 키워드를 통한 도서 검색 : GET "/books/search?keyword=" - ISBN 바코드 인식을 통한 도서 검색 : GET "/books/search/isbn?isbn="

---
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/book/presentation/BookController.java
@@ -0,0 +1,31 @@
+package kr.mybrary.bookservice.book.presentation;
+
+import java.util.List;
+import kr.mybrary.bookservice.book.domain.PlatformBookSearchApiService;
+import kr.mybrary.bookservice.book.presentation.dto.response.BookSearchResultResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books")
+public class BookController {
+
+    private final PlatformBookSearchApiService bookService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<BookSearchResultResponse>> searchWithKeyword(
+            @RequestParam("keyword") String keyword) {
+        return ResponseEntity.ok(bookService.searchWithKeyWord(keyword));
+    }
+
+    @GetMapping("/search/isbn")
+    public ResponseEntity<List<BookSearchResultResponse>> searchWithISBNBarcodeScan(
+            @RequestParam("isbn") String isbn) {
+        return ResponseEntity.ok(bookService.searchWithISBN(isbn));
+    }
+}
