85e0ab00fa73264e7d7c1bcd23d4d35dc493aacc
minseong <kdsvip5@naver.com>
2023-06-25 01:17:07 +0900

    [IDLE-83] feat#88: 예외 발생시 Http Status를 지정할 수 있도록 코드 수정 - 커스텀 예외에 status 필드 추가 - ControllerAdvice의 리턴값 ResponseEntity에 statusCode 지정

--- backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/exception/BookSearchResultNotFoundException.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/exception/BookSearchResultNotFoundException.java
@@ -2,12 +2,13 @@ package kr.mybrary.bookservice.book.domain.exception;
 
 import kr.mybrary.bookservice.global.exception.ApplicationException;
 
-public class BookNotFoundWithWrongInputException extends ApplicationException {
+public class BookSearchResultNotFoundException extends ApplicationException {
 
+    private final static int STATUS = 404;
     private final static String ERROR_CODE = "B-01";
     private final static String ERROR_MESSAGE = "잘못된 검색으로 찾을 수 없는 도서입니다.";
 
-    public BookNotFoundWithWrongInputException() {
-        super(ERROR_CODE, ERROR_MESSAGE);
+    public BookSearchResultNotFoundException() {
+        super(STATUS, ERROR_CODE, ERROR_MESSAGE);
     }
 }
