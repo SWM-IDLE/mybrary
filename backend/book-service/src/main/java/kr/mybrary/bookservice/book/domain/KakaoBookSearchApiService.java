d66c4058057e832a6437cc0d0ad831679cd762f9
minseong <kdsvip5@naver.com>
2023-06-25 01:53:13 +0900

    [IDLE-83] refactor#88: BookSearchApiService 리펙토링 - 단순 String 상수화 - 메서드 내 공통 로직 함수로 추출 - 인터페이스의 리턴값을 Object가 아닌 List<Dto>로 수정

--- backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/KakaoBookSearchApiService.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/KakaoBookSearchApiService.java
@@ -1,6 +1,11 @@
 package kr.mybrary.bookservice.book.domain;
 
+import java.util.List;
+import kr.mybrary.bookservice.book.domain.dto.BookDtoMapper;
+import kr.mybrary.bookservice.book.domain.dto.kakaoapi.Document;
 import kr.mybrary.bookservice.book.domain.dto.kakaoapi.KakaoBookSearchResponse;
+import kr.mybrary.bookservice.book.domain.exception.BookSearchResultNotFoundException;
+import kr.mybrary.bookservice.book.presentation.dto.response.BookSearchResultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -20,41 +25,40 @@ public class KakaoBookSearchApiService implements PlatformBookSearchApiService {
     @Value("${kakao.api.key}")
     private String API_KEY;
 
-    private final String API_URL_WITH_KEYWORD = "https://dapi.kakao.com/v3/search/book?query=%s";
-    private final String API_URL_WITH_ISBN = "https://dapi.kakao.com/v3/search/book?target=isbn&query=%s";
+    private static final String API_URL_WITH_KEYWORD = "https://dapi.kakao.com/v3/search/book?query=%s";
+    private static final String API_URL_WITH_ISBN = "https://dapi.kakao.com/v3/search/book?target=isbn&query=%s";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String KAKAO_AUTHORIZATION_HEADER_PREFIX = "KakaoAK ";
 
     @Override
-    public KakaoBookSearchResponse searchWithKeyWord(String keyword) {
-
-        HttpEntity<HttpHeaders> httpEntity = createHttpEntity();
-
-        String requestUri = String.format(API_URL_WITH_KEYWORD, keyword);
-        ResponseEntity<KakaoBookSearchResponse> exchange = restTemplate.exchange(requestUri,
-                HttpMethod.GET, httpEntity,
-                KakaoBookSearchResponse.class);
-
-        return exchange.getBody();
+    public List<BookSearchResultResponse> searchWithKeyWord(String keyword) {
+        return searchBookFromKakaoApi(API_URL_WITH_KEYWORD, keyword);
     }
 
     @Override
-    public KakaoBookSearchResponse searchWithISBN(String isbn) {
-
-        HttpEntity<HttpHeaders> httpEntity = createHttpEntity();
-
-        String requestUri = String.format(API_URL_WITH_ISBN, isbn);
-        ResponseEntity<KakaoBookSearchResponse> exchange = restTemplate.exchange(requestUri,
-                HttpMethod.GET, httpEntity,
-                KakaoBookSearchResponse.class);
-
-        return exchange.getBody();
+    public List<BookSearchResultResponse> searchWithISBN(String isbn) {
+        return searchBookFromKakaoApi(API_URL_WITH_ISBN, isbn);
     }
 
-    private HttpEntity<HttpHeaders> createHttpEntity() {
+    private List<BookSearchResultResponse> searchBookFromKakaoApi(String baseUrl, String searchKeyword) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "KakaoAK " + API_KEY);
+        headers.add(AUTHORIZATION_HEADER, KAKAO_AUTHORIZATION_HEADER_PREFIX + API_KEY);
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<HttpHeaders> httpEntity = new HttpEntity<>(headers);
-        return httpEntity;
+
+        String requestUri = String.format(baseUrl, searchKeyword);
+        ResponseEntity<KakaoBookSearchResponse> response = restTemplate.exchange(requestUri,
+                HttpMethod.GET, httpEntity, KakaoBookSearchResponse.class);
+
+        List<Document> documents = response.getBody().getDocuments();
+
+        if (documents.isEmpty()) {
+            throw new BookSearchResultNotFoundException();
+        }
+
+        return documents.stream()
+                .map(BookDtoMapper.INSTANCE::kakaoSearchResponseToResponseDto)
+                .toList();
     }
 }
