45349f5c4743a029f7b13e3a4b7f36337df82417
0112leesy <0112leesy@naver.com>
2023-06-28 16:26:08 +0900

    [IDLE-60] chore: ExceptionHandler 이름 변경, http 패키지 생성
      - customException -> applicationException, runtimeException
      - 최상단 디렉토리에 http 패키지 생성

--- user-service/src/main/java/kr/mybrary/userservice/global/exception/GlobalControllerAdvice.java
+++ user-service/src/main/java/kr/mybrary/userservice/global/exception/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package kr.mybrary.userservice.global.exception;
 
+import javax.security.sasl.AuthenticationException;
 import kr.mybrary.userservice.global.dto.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(ApplicationException.class)
-    public ResponseEntity<ErrorResponse> customException(ApplicationException e) {
+    public ResponseEntity<ErrorResponse> applicationException(ApplicationException e) {
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of(e.getErrorCode(), e.getErrorMessage()));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> customException(RuntimeException e) {
+    public ResponseEntity<ErrorResponse> runtimeException(RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of("RT-01", e.getMessage()));
     }
