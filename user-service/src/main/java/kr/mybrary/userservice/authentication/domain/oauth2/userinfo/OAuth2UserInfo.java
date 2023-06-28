01693b7c51f38e3c6d07e297f51377eb42c25f1c
0112leesy <0112leesy@naver.com>
2023-06-28 23:38:14 +0900

    [IDLE-59] feat: OAuth2 설정 및 OAuth2UserInfo 생성
      - application-oauth.yml 설정 및 oauth2 의존성 주입
      - authentication/domain/ 하위에 oauth2 패키지 생성
      - oauth2/ 하위에 userinfo 패키지 생성
      - 소셜 타입 별 유저 정보를 가지는 OAuth2userInfo 추상 클래스 생성
        - GoogleOAuth2UserInfo 구현체 생성

---
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/domain/oauth2/userinfo/OAuth2UserInfo.java
@@ -0,0 +1,19 @@
+package kr.mybrary.userservice.authentication.domain.oauth2.userinfo;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getId();
+
+    public abstract String getNickname();
+
+    public abstract String getImageUrl();
+
+}
