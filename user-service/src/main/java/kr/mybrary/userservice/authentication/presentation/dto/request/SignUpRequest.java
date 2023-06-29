717da1fffb79dc0628ada7da6817b28423fc004e
0112leesy <0112leesy@naver.com>
2023-06-30 02:18:17 +0900

    [IDLE-59] feat: 소셜 로그인 성공 시 RefreshToken DB에 저장
      - AccessToken, RefreshToken을 생성해서 헤더에 넣는 로직을 함수로 빼서 구성
      - 회원 가입 시에도 RefreshToken이 DB에 저장되도록 함
    
      - SignUpRequest에서 닉네임 형식 변경
        - 기존: 특수 문자 제외 2~10자 (대문자 불가)
        - 변경: 특수 문자 제외 2~20자 (대문자 가능)

--- user-service/src/main/java/kr/mybrary/userservice/authentication/presentation/dto/request/SignUpRequest.java
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/presentation/dto/request/SignUpRequest.java
@@ -22,7 +22,7 @@ public class SignUpRequest {
 
     @NotNull
     @NotBlank(message = "닉네임은 필수입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,20}$", message = "닉네임은 특수문자를 제외한 2~20자리여야 합니다.")
     private String nickname;
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
