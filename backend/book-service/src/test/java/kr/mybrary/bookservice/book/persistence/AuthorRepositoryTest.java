a1d81ab610260dac7c9e6a0573a578715065f95e
minseong <kdsvip5@naver.com>
2023-06-30 23:11:08 +0900

    [IDLE-96] test#99: Book 도메인 테스트 코드 작성 - Presentation Layer Test - @WebMvcTest, @MockBean 사용 - Domain Layer Test - @Mock, @InjectMocks 사용 - Presentation Layer Test - @DataJpaTest 사용

---
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/book/persistence/AuthorRepositoryTest.java
@@ -0,0 +1,51 @@
+package kr.mybrary.bookservice.book.persistence;
+
+import static org.assertj.core.api.Assertions.*;
+
+import kr.mybrary.bookservice.book.persistence.author.Author;
+import kr.mybrary.bookservice.book.persistence.repository.AuthorRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AuthorRepositoryTest {
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @DisplayName("저자를 저장한다.")
+    @Test
+    void saveAuthor() {
+        // given
+        Author author = createAuthor();
+
+        // when
+        Author savedAuthor = authorRepository.save(author);
+
+        // then
+        assertThat(savedAuthor.getName()).isEqualTo(author.getName());
+    }
+
+    @DisplayName("저자를 이름으로 조회한다.")
+    @Test
+    void findAuthorByName() {
+        // given
+        Author savedAuthor = authorRepository.save(createAuthor());
+
+        // when
+        Author foundAuthor = authorRepository.findByName(savedAuthor.getName()).orElseThrow();
+
+        // then
+        assertThat(foundAuthor.getName()).isEqualTo(savedAuthor.getName());
+    }
+
+    private Author createAuthor() {
+        return Author.builder()
+                .name("author_name")
+                .build();
+    }
+}
---
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/book/presentation/BookControllerTest.java
@@ -0,0 +1,68 @@
+package kr.mybrary.bookservice.book.presentation;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import java.util.List;
+import kr.mybrary.bookservice.book.domain.BookService;
+import kr.mybrary.bookservice.book.persistence.Book;
+import kr.mybrary.bookservice.book.presentation.dto.request.BookCreateRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(BookController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+class BookControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @DisplayName("새로운 도서를 등록한다.")
+    @Test
+    void create() throws Exception {
+        // given
+        BookCreateRequest request = createBookCreateRequest();
+
+        String requestJson = objectMapper.writeValueAsString(request);
+
+        given(bookService.getRegisteredOrNewBook(request.toServiceRequest()))
+                .willReturn(any(Book.class));
+
+        // when, then
+        mockMvc.perform(post("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isCreated());
+    }
+
+    private BookCreateRequest createBookCreateRequest() {
+        return BookCreateRequest.builder()
+                .title("title")
+                .description("description")
+                .isbn10("isbn10")
+                .isbn13("isbn13")
+                .publisher("publisher")
+                .price(10000)
+                .publicationDate(LocalDateTime.now())
+                .translators(List.of("translator1", "translator2"))
+                .authors(List.of("author1", "author2"))
+                .thumbnailUrl("thumbnailUrl")
+                .build();
+    }
+}
