bf7dc7fe034949c02d3d20040aa23c9039eb15b2
minseong <kdsvip5@naver.com>
2023-06-30 22:36:34 +0900

    [IDLE-96] refactor: book 패키지의 Presentation Layer와 Domain Layer의 DTO 분리

--- backend/book-service/src/main/java/kr/mybrary/bookservice/book/presentation/dto/request/BookCreateRequest.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/book/presentation/dto/request/BookCreateRequest.java
@@ -2,6 +2,7 @@ package kr.mybrary.bookservice.book.presentation.dto.request;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import kr.mybrary.bookservice.book.domain.dto.request.BookCreateServiceRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,4 +23,19 @@ public class BookCreateRequest {
 
     private List<String> authors;
     private List<String> translators;
+
+    public BookCreateServiceRequest toServiceRequest() {
+        return BookCreateServiceRequest.builder()
+                .title(title)
+                .description(description)
+                .isbn10(isbn10)
+                .isbn13(isbn13)
+                .publisher(publisher)
+                .publicationDate(publicationDate)
+                .price(price)
+                .thumbnailUrl(thumbnailUrl)
+                .authors(authors)
+                .translators(translators)
+                .build();
+    }
 }
