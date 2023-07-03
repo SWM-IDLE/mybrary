28c07ed2ef4b1d5157b77538af8646f603b541fe
minseong <kdsvip5@naver.com>
2023-07-03 15:38:53 +0900

    apigateway-server initial commit

---
+++ backend/apigateway-server/src/test/java/kr/mybrary/apigatewayserver/ApigatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mybrary.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
