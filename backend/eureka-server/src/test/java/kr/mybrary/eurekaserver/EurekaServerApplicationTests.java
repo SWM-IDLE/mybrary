6f252e4254be0cbee3223c752acfd2e2aa1c15f6
minseong <kdsvip5@naver.com>
2023-07-03 14:54:07 +0900

    eureka-server initial commit

---
+++ backend/eureka-server/src/test/java/kr/mybrary/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mybrary.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
