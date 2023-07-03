0665a437c83c1f77f6b0dee62616cf9e608ca38f
minseong <kdsvip5@naver.com>
2023-07-03 15:38:53 +0900

    apigateway-server initial commit

---
+++ backend/apigateway-server/src/main/java/kr/mybrary/apigatewayserver/ApigatewayServerApplication.java
@@ -0,0 +1,13 @@
+package kr.mybrary.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayServerApplication.class, args);
+	}
+
+}
