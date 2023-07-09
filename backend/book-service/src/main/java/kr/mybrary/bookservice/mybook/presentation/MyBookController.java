4b597aba1a7f01270e71ac4e5f83622bff98c11a
minseong <kdsvip5@naver.com>
2023-07-09 22:45:39 +0900

    [IDLE-111] feat: MybookController GET "/api/v1/users/{userId}/mybooks" EndPoint 추가 - HTTP Request Header를 통해 로그인한 유저 아이디, PathVariable로 찾고자하는 마이북의 유저 아디이 전달받음.

--- backend/book-service/src/main/java/kr/mybrary/bookservice/mybook/presentation/MyBookController.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/mybook/presentation/MyBookController.java
@@ -1,8 +1,11 @@
 package kr.mybrary.bookservice.mybook.presentation;
 
+import java.util.List;
 import kr.mybrary.bookservice.global.dto.response.SuccessResponse;
 import kr.mybrary.bookservice.mybook.domain.MyBookService;
+import kr.mybrary.bookservice.mybook.domain.dto.request.MyBookFindAllServiceRequest;
 import kr.mybrary.bookservice.mybook.presentation.dto.request.MyBookCreateRequest;
+import kr.mybrary.bookservice.mybook.presentation.dto.response.MyBookElementResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/mybooks")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class MyBookController {
 
     private final MyBookService myBookService;
 
-    @PostMapping
+    @PostMapping("/mybooks")
     public ResponseEntity createMyBook(@RequestBody MyBookCreateRequest request) {
 
         String userId = "test1"; // TODO: 임시 회원 ID
@@ -31,16 +35,16 @@ public class MyBookController {
                 .body(SuccessResponse.of(HttpStatus.CREATED.toString(), "내 서재에 도서를 등록했습니다.", null));
     }
 
-    @GetMapping
-    public ResponseEntity findAllMyBooks() {
+    @GetMapping("/users/{userId}/mybooks")
+    public ResponseEntity findAllMyBooks(@RequestHeader("USER-ID") String loginId, @PathVariable("userId") String userId) {
 
-        String userId = "test1"; // TODO: 임시 회원 ID
+        List<MyBookElementResponse> myBooks = myBookService.findAllMyBooks(
+                MyBookFindAllServiceRequest.of(userId, loginId));
 
-        return ResponseEntity.ok(
-                SuccessResponse.of(HttpStatus.OK.toString(), "내 서재의 도서 목록입니다.", myBookService.findAllMyBooks(userId)));
+        return ResponseEntity.ok(SuccessResponse.of(HttpStatus.OK.toString(), "서재의 도서 목록입니다.", myBooks));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/mybooks/{id}")
     public ResponseEntity findMyBookDetail(@PathVariable Long id) {
 
         String userId = "test1"; // TODO: 임시 회원 ID
@@ -49,7 +53,7 @@ public class MyBookController {
                 SuccessResponse.of(HttpStatus.OK.toString(), "마이북 상세보기입니다.", myBookService.findMyBookDetail(userId, id)));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/mybooks/{id}")
     public ResponseEntity deleteMyBook(@PathVariable Long id) {
 
         String userId = "test1"; // TODO: 임시 회원 ID
