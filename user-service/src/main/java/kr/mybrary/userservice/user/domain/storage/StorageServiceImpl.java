9e12222e74118ac25ca59fe82fa82474883b26af
0112leesy <0112leesy@naver.com>
2023-07-13 20:08:36 +0900

    [IDLE-106] feat: AmazonS3 dependency 변경
      - 기존) spring-cloud-starter-aws:2.3.1
      - 변경) spring-cloud-aws-starter-s3:3.0.1
      - 개선점) Spring Cloud AWS 공식 문서에 명시된 버전으로 변경 (SpringBoot 3 호환)
      - S3Client Bean을 자동으로 생성해주기 때문에 AmazonS3Config 파일 삭제

--- user-service/src/main/java/kr/mybrary/userservice/user/domain/storage/StorageServiceImpl.java
+++ user-service/src/main/java/kr/mybrary/userservice/user/domain/storage/StorageServiceImpl.java
@@ -1,9 +1,8 @@
 package kr.mybrary.userservice.user.domain.storage;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Exception;
+import io.awspring.cloud.s3.S3Template;
 import java.io.IOException;
 import kr.mybrary.userservice.user.domain.exception.file.FileInputStreamException;
 import kr.mybrary.userservice.user.domain.exception.storage.StorageClientException;
@@ -18,37 +17,37 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class StorageServiceImpl implements StorageService {
     // TODO: LocalStack을 사용하여 테스트하기
-
-    private final AmazonS3Client amazonS3Client;
-
+    private final S3Template amazonS3Client;
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucketName;
+    private static final String BASE_PATH = "https://mybrary-user-service.s3.ap-northeast-2.amazonaws.com/";
 
     @Override
     public String putFile(MultipartFile multipartFile, String path, String fileName) {
+        String profileImageFilePath = generateProfileImageFilePath(path, fileName, multipartFile);
         try {
-            amazonS3Client.putObject(generatePutObjectRequest(multipartFile, path, fileName));
-        } catch (AmazonS3Exception e) {
+            amazonS3Client.upload(
+                    bucketName,
+                    profileImageFilePath,
+                    multipartFile.getInputStream(),
+                    generateObjectMetadata(multipartFile)
+            );
+        } catch (S3Exception e) {
             throw new StorageClientException();
         } catch (IOException e) {
             throw new FileInputStreamException();
         }
-        return amazonS3Client.getUrl(bucketName, path + "/" + fileName).toString();
+        return BASE_PATH + profileImageFilePath;
     }
 
-    private PutObjectRequest generatePutObjectRequest(MultipartFile multipartFile, String path,
-            String fileName) throws IOException {
-        ObjectMetadata objectMetadata = generateObjectMetadata(multipartFile);
-        String storagePath = path + "/" + fileName;
-        return new PutObjectRequest(bucketName, storagePath, multipartFile.getInputStream(),
-                objectMetadata);
+    private static String generateProfileImageFilePath(String path, String fileName, MultipartFile multipartFile) {
+        return path + "/" + fileName + "." + multipartFile.getContentType().split("/")[1];
     }
 
     private static ObjectMetadata generateObjectMetadata(MultipartFile multipartFile) {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        objectMetadata.setContentType(multipartFile.getContentType());
-        objectMetadata.setContentLength(multipartFile.getSize());
-        return objectMetadata;
+        return ObjectMetadata.builder()
+                .contentType(multipartFile.getContentType())
+                .build();
     }
 
 }
