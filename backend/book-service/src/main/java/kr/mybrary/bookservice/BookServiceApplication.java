2fd2433bd3da5e43767c423fdedbf37e9deb9870
minseong <kdsvip5@naver.com>
2023-07-13 21:56:49 +0900

    [IDLE-127] config: eureka-client 설정

--- backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
@@ -4,11 +4,13 @@ import java.time.Duration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
