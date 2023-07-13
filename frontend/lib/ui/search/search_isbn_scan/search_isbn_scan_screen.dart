f77fd92d87832ee95ee2d4fee222034c3090e0c6
DonggyuJin <jdk2531@gmail.com>
2023-07-14 00:14:41 +0900

    [IDLE-117] feat&refactor: 권한 설정 구현 및 리팩터링 진행 - 권한 요청 관련 패키지 사용 및 카메라 권한 요청 구현 - 권한 없을 시, SnackBar 생성 및 설정 유도 - Android&iOS 앱 이름 및 권한 요청 내용 다국화(ko,en) - 스캔 페이지 관련 페이지 분리

---
+++ frontend/lib/ui/search/search_isbn_scan/search_isbn_scan_screen.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:mybrary/data/datasource/remote_datasource.dart';
+import 'package:mybrary/data/model/search/book_search_data.dart';
+import 'package:mybrary/data/model/search/book_search_response.dart';
+import 'package:mybrary/res/colors/color.dart';
+import 'package:mybrary/ui/search/search_detail/search_detail_screen.dart';
+import 'package:mybrary/ui/search/search_isbn_scan/components/isbn_scan_appbar.dart';
+import 'package:mybrary/ui/search/search_isbn_scan/components/isbn_scan_box.dart';
+import 'package:mybrary/ui/search/search_isbn_scan/components/isbn_scan_description.dart';
+import 'package:mybrary/ui/search/search_isbn_scan/components/isbn_scan_header.dart';
+
+class SearchIsbnScanScreen extends StatefulWidget {
+  const SearchIsbnScanScreen({super.key});
+
+  @override
+  State<SearchIsbnScanScreen> createState() => _SearchIsbnScanScreenState();
+}
+
+class _SearchIsbnScanScreenState extends State<SearchIsbnScanScreen> {
+  BookSearchData _searchBookIsbnInfo = BookSearchData.fromJson({});
+  MobileScannerController cameraController = MobileScannerController(
+    detectionSpeed: DetectionSpeed.normal,
+    facing: CameraFacing.back,
+    torchEnabled: false,
+  );
+
+  @override
+  void initState() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: MYBOOK_SCAN_BACKGROUND_COLOR,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: MYBOOK_SCAN_BACKGROUND_COLOR,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: LESS_GREY_COLOR.withOpacity(0.2),
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: IsbnScanAppBar(
+        appBar: AppBar(),
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SizedBox(
+        height: height,
+        child: Stack(
+          children: [
+            SizedBox(
+              width: width,
+              child: MobileScanner(
+                controller: cameraController,
+                scanWindow: Rect.fromLTWH(0, 150, width * 0.8, height * 0.35),
+                onDetect: (capture) {
+                  final List<Barcode> barcodes = capture.barcodes;
+                  for (final barcode in barcodes) {
+                    if (barcode.rawValue != null) {
+                      setState(() {
+                        _fetchBookSearchIsbnResponse(barcode.rawValue!)
+                            .then((value) {
+                          _searchBookIsbnInfo = value;
+                        });
+                      });
+                    }
+
+                    if (_searchBookIsbnInfo.title != null) {
+                      onNavigateToSearchDetailScreen();
+                      cameraController.dispose();
+                    }
+                  }
+                },
+              ),
+            ),
+            IsbnScanHeader(
+              width: width,
+              height: height,
+            ),
+            IsbnScanDescription(
+              width: width,
+              height: height,
+            ),
+            IsbnScanBox(
+              width: width,
+              height: height,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onNavigateToSearchDetailScreen() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (_) => SearchDetailScreen(
+          searchBookData: _searchBookIsbnInfo,
+        ),
+      ),
+    );
+  }
+
+  Future<BookSearchData> _fetchBookSearchIsbnResponse(String isbn) async {
+    BookSearchResponse bookSearchResponse =
+        await RemoteDataSource.getBookSearchKeywordResponse(
+            BookSearchRequestType.searchIsbnScan, isbn);
+
+    final bookSearchData = bookSearchResponse.data!.bookSearchResult![0];
+
+    return bookSearchData;
+  }
+}
