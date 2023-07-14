b0236ad3c59252cc5096cb59fb495147df66c09b
0112leesy <0112leesy@naver.com>
2023-07-15 03:06:05 +0900

    [IDLE-107] test: 팔로우, 언팔로우, 팔로워 삭제 테스트
      - 팔로우, 언팔로우 테스트 시 팔로워, 팔로잉 목록에 담긴 객체의 동등성 비교가 어렵기 때문에 UserFixture 대신 User 객체를 직접 생성하여 테스트
      - TestMethodOrder을 사용해 순차적으로 기능 테스트

---
+++ user-service/src/test/java/kr/mybrary/userservice/user/domain/follow/FollowTest.java
@@ -0,0 +1,244 @@
+package kr.mybrary.userservice.user.domain.follow;
+
+import kr.mybrary.userservice.user.domain.UserServiceImpl;
+import kr.mybrary.userservice.user.domain.dto.request.FollowServiceRequest;
+import kr.mybrary.userservice.user.domain.dto.request.FollowerServiceRequest;
+import kr.mybrary.userservice.user.domain.dto.response.FollowerServiceResponse;
+import kr.mybrary.userservice.user.domain.dto.response.FollowingServiceResponse;
+import kr.mybrary.userservice.user.domain.exception.follow.DuplicateFollowException;
+import kr.mybrary.userservice.user.domain.exception.follow.SameSourceTargetUserException;
+import kr.mybrary.userservice.user.domain.exception.user.UserNotFoundException;
+import kr.mybrary.userservice.user.persistence.User;
+import kr.mybrary.userservice.user.persistence.repository.UserRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class FollowTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userService;
+
+    static User userA = User.builder()
+            .loginId("userA")
+            .followers(new ArrayList<>())
+            .followings(new ArrayList<>())
+            .build();
+
+    static User userB = User.builder()
+            .loginId("userB")
+            .followers(new ArrayList<>())
+            .followings(new ArrayList<>())
+            .build();
+
+    static User userC = User.builder()
+            .loginId("userC")
+            .followers(new ArrayList<>())
+            .followings(new ArrayList<>())
+            .build();
+
+    @Test
+    @Order(1)
+    @DisplayName("userA는 userB를 팔로우한다.")
+    void userAFollowUserB() {
+        // given
+        given(userRepository.findByLoginId(userA.getLoginId())).willReturn(Optional.of(userA));
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+
+        // when
+        userService.follow(FollowServiceRequest.of(userA.getLoginId(), userB.getLoginId()));
+
+        // then
+        assertAll(
+                () -> assertThat(userA.getFollowings()).hasSize(1),
+                () -> assertThat(userA.getFollowings().get(0).getSource()).isEqualTo(userA),
+                () -> assertThat(userA.getFollowings().get(0).getTarget()).isEqualTo(userB),
+                () -> assertThat(userB.getFollowers()).hasSize(1),
+                () -> assertThat(userB.getFollowers().get(0).getSource()).isEqualTo(userA),
+                () -> assertThat(userB.getFollowers().get(0).getTarget()).isEqualTo(userB)
+        );
+
+        verify(userRepository).findByLoginId(userA.getLoginId());
+        verify(userRepository).findByLoginId(userB.getLoginId());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("userA의 팔로잉 목록을 조회한다.")
+    void getUserAFollowings() {
+        // given
+        given(userRepository.findByLoginId(userA.getLoginId())).willReturn(Optional.of(userA));
+
+        // when
+        FollowingServiceResponse response = userService.getFollowings(userA.getLoginId());
+
+        // then
+        assertAll(
+                () -> assertThat(response.getRequestLoginId()).isEqualTo(userA.getLoginId()),
+                () -> assertThat(response.getFollowings()).hasSize(1),
+                () -> assertThat(response.getFollowings()).extracting("loginId").contains(userB.getLoginId())
+        );
+
+        verify(userRepository).findByLoginId(userA.getLoginId());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("userC는 userB를 팔로우한다.")
+    void userCFollowUserB() {
+        // given
+        given(userRepository.findByLoginId(userC.getLoginId())).willReturn(Optional.of(userC));
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+
+        // when
+        userService.follow(FollowServiceRequest.of(userC.getLoginId(), userB.getLoginId()));
+
+        // then
+        assertAll(
+                () -> assertThat(userC.getFollowings()).hasSize(1),
+                () -> assertThat(userC.getFollowings().get(0).getSource()).isEqualTo(userC),
+                () -> assertThat(userC.getFollowings().get(0).getTarget()).isEqualTo(userB),
+                () -> assertThat(userB.getFollowers()).hasSize(2),
+                () -> assertThat(userB.getFollowers().get(1).getSource()).isEqualTo(userC),
+                () -> assertThat(userB.getFollowers().get(1).getTarget()).isEqualTo(userB)
+        );
+
+        verify(userRepository).findByLoginId(userC.getLoginId());
+        verify(userRepository).findByLoginId(userB.getLoginId());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("userC가 userB를 다시 팔로우하면 예외가 발생한다.")
+    void userCFollowUserBAgain() {
+        // given
+        given(userRepository.findByLoginId(userC.getLoginId())).willReturn(Optional.of(userC));
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+
+        // when then
+        assertThatThrownBy(() -> userService.follow(FollowServiceRequest.of(userC.getLoginId(), userB.getLoginId())))
+                .isInstanceOf(DuplicateFollowException.class)
+                .hasFieldOrPropertyWithValue("status", 400)
+                .hasFieldOrPropertyWithValue("errorCode", "F-02")
+                .hasFieldOrPropertyWithValue("errorMessage", "이미 팔로우한 사용자입니다.");
+
+        verify(userRepository).findByLoginId(userC.getLoginId());
+        verify(userRepository).findByLoginId(userB.getLoginId());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("userB의 팔로워 목록을 조회한다.")
+    void getUserBFollowers() {
+        // given
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+
+        // when
+        FollowerServiceResponse response = userService.getFollowers(userB.getLoginId());
+
+        // then
+        assertAll(
+                () -> assertThat(response.getRequestLoginId()).isEqualTo(userB.getLoginId()),
+                () -> assertThat(response.getFollowers()).hasSize(2),
+                () -> assertThat(response.getFollowers()).extracting("loginId").containsExactly(userA.getLoginId(), userC.getLoginId())
+        );
+
+        verify(userRepository).findByLoginId(userB.getLoginId());
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("userA는 userB를 언팔로우한다.")
+    void userAUnfollowUserB() {
+        // given
+        given(userRepository.findByLoginId(userA.getLoginId())).willReturn(Optional.of(userA));
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+
+        // when
+        userService.unfollow(FollowServiceRequest.of(userA.getLoginId(), userB.getLoginId()));
+
+        // then
+        assertAll(
+                () -> assertThat(userA.getFollowings()).hasSize(0),
+                () -> assertThat(userB.getFollowers()).hasSize(1)
+        );
+
+        verify(userRepository).findByLoginId(userA.getLoginId());
+        verify(userRepository).findByLoginId(userB.getLoginId());
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("userB는 userC를 팔로워 목록에서 삭제한다.")
+    void userBDeletefollowerUserC() {
+        // given
+        given(userRepository.findByLoginId(userB.getLoginId())).willReturn(Optional.of(userB));
+        given(userRepository.findByLoginId(userC.getLoginId())).willReturn(Optional.of(userC));
+
+        // when
+        userService.deleteFollower(FollowerServiceRequest.of(userB.getLoginId(), userC.getLoginId()));
+
+        // then
+        assertAll(
+                () -> assertThat(userB.getFollowers()).hasSize(0),
+                () -> assertThat(userC.getFollowings()).hasSize(0)
+        );
+
+        verify(userRepository).findByLoginId(userB.getLoginId());
+        verify(userRepository).findByLoginId(userC.getLoginId());
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("userA가 userA 자신을 팔로우하면 예외가 발생한다.")
+    void userAFollowUserA() {
+        // given
+        given(userRepository.findByLoginId(userA.getLoginId())).willReturn(Optional.of(userA));
+
+        // when then
+        assertThatThrownBy(() -> userService.follow(FollowServiceRequest.of(userA.getLoginId(), userA.getLoginId())))
+                .isInstanceOf(SameSourceTargetUserException.class)
+                .hasFieldOrPropertyWithValue("status", 400)
+                .hasFieldOrPropertyWithValue("errorCode", "F-01")
+                .hasFieldOrPropertyWithValue("errorMessage", "자기 자신을 팔로우 또는 언팔로우할 수 없습니다.");
+
+        verify(userRepository, times(2)).findByLoginId(userA.getLoginId());
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("userA가 없는 사용자를 팔로우하면 예외가 발생한다.")
+    void userAFollowNonExistUser() {
+        // given
+        given(userRepository.findByLoginId(userA.getLoginId())).willReturn(Optional.of(userA));
+        given(userRepository.findByLoginId("nonExistUser")).willReturn(Optional.empty());
+
+        // when
+        assertThatThrownBy(() -> userService.follow(FollowServiceRequest.of(userA.getLoginId(), "nonExistUser")))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasFieldOrPropertyWithValue("status", 404)
+                .hasFieldOrPropertyWithValue("errorCode", "U-05")
+                .hasFieldOrPropertyWithValue("errorMessage", "존재하지 않는 사용자입니다.");
+
+        verify(userRepository).findByLoginId(userA.getLoginId());
+        verify(userRepository).findByLoginId("nonExistUser");
+    }
+
+}
