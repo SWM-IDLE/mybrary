dbfb47b793659ed5fc9f98521f420cd23ec998f7
0112leesy <0112leesy@naver.com>
2023-07-15 00:56:11 +0900

    [IDLE-107] feat: 팔로잉 조회 기능 구현
      - chore: SameSourceTargetUserException 오류 메시지 수정

--- user-service/src/main/java/kr/mybrary/userservice/user/domain/exception/follow/SameSourceTargetUserException.java
+++ user-service/src/main/java/kr/mybrary/userservice/user/domain/exception/follow/SameSourceTargetUserException.java
@@ -6,7 +6,7 @@ public class SameSourceTargetUserException extends ApplicationException {
 
         private final static int STATUS = 400;
         private final static String ERROR_CODE = "F-01";
-        private final static String ERROR_MESSAGE = "자기 자신을 팔로우할 수 없습니다.";
+        private final static String ERROR_MESSAGE = "자기 자신을 팔로우 또는 언팔로우할 수 없습니다.";
 
         public SameSourceTargetUserException() {
             super(STATUS, ERROR_CODE, ERROR_MESSAGE);
