f43b6fde422521e17750cdac12264a97c9ccee11
minseong <kdsvip5@naver.com>
2023-07-23 00:29:21 +0900

    [IDLE-133] feat: MeaningTagService 의미 태그 페이징 조회 메서드 구현 - size 를 전달받아, 페이징 처리한 데이터 리스트를 반환한다. - 테스트 코드 작성 - MeaningTagDtoMapper 추가

--- backend/book-service/src/main/java/kr/mybrary/bookservice/tag/domain/MeaningTagService.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/tag/domain/MeaningTagService.java
@@ -1,11 +1,16 @@
 package kr.mybrary.bookservice.tag.domain;
 
+import java.util.List;
+import kr.mybrary.bookservice.tag.domain.dto.MeaningTagDtoMapper;
 import kr.mybrary.bookservice.tag.domain.dto.request.MeaningTagAssignServiceRequest;
+import kr.mybrary.bookservice.tag.domain.dto.request.MeaningTagFindPageServiceRequest;
 import kr.mybrary.bookservice.tag.persistence.repository.MyBookMeaningTagRepository;
 import kr.mybrary.bookservice.tag.persistence.MeaningTag;
 import kr.mybrary.bookservice.tag.persistence.repository.MeaningTagRepository;
 import kr.mybrary.bookservice.tag.persistence.MyBookMeaningTag;
+import kr.mybrary.bookservice.tag.presentation.dto.response.MeaningTagElementResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +21,17 @@ public class MeaningTagService {
 
     private final MeaningTagRepository meaningTagRepository;
     private final MyBookMeaningTagRepository myBookMeaningTagRepository;
+
+    @Transactional(readOnly = true)
+    public List<MeaningTagElementResponse> findPageOrderByRegisteredCount(
+            MeaningTagFindPageServiceRequest request) {
+
+        PageRequest pageRequest = PageRequest.of(0, request.getSize());
+
+        return meaningTagRepository.findAllByOrderByRegisteredCountDesc(pageRequest)
+                .map(MeaningTagDtoMapper.INSTANCE::entityToMeaningTagElementResponse)
+                .toList();
+    }
 
     public void assignMeaningTag(MeaningTagAssignServiceRequest request) {
         MeaningTag meaningTag = meaningTagRepository.findByQuote(request.getQuote())
