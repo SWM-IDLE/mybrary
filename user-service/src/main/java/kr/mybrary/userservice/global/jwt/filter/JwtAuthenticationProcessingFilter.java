e2f206685900f8ebe35806be2fd844c3abd87daf
0112leesy <0112leesy@naver.com>
2023-07-22 23:00:18 +0900

    [IDLE-62] refactor: JwtAuthenticationProcessingFilter 리팩터링
      - 주석 관리
      - return문 -> if-else문으로 변경
      - 토큰 검증이 불필요한 url인지 확인하는 부분 함수화

--- user-service/src/main/java/kr/mybrary/userservice/global/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ user-service/src/main/java/kr/mybrary/userservice/global/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -44,52 +44,50 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        if (request.getRequestURI().contains(USER_SERVICE_URL) || request.getRequestURI().contains(LOGIN_URL) ||
-                request.getRequestURI().contains(OAUTH2_URL)) {
+        if (tokenAuthenticationNotRequired(request)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        // 요청 헤더에서 AccessToken 추출 - 없거나 유효하지 않으면 null
         String accessToken = jwtService.extractAccessToken(request).orElse(null);
-
-        // Redis 블랙리스트에서 accessToken 확인
         checkIfTokenIsLogout(accessToken);
-
         String loginId = jwtService.getLoginId(accessToken).orElse(null);
 
-        // 요청 헤더에서 RefreshToken 추출 - 없거나 유효하지 않으면 null
         String refreshToken = jwtService.extractRefreshToken(request).orElse(null);
 
-        // RefreshToken이 존재하면 AccessToken 재발급 - 인증 처리는 하지 않음
-        if (refreshToken != null) {
+        /* RefreshToken이 없거나 유효하지 않다면 AccessToken을 검사하고 인증 처리
+           AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
+           AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
+           RefreshToken이 존재하면 AccessToken 재발급 - 인증 처리는 하지 않음 */
+        if (refreshToken == null) {
+            saveAuthentication(getUserWithAccessToken(accessToken));
+            request.setAttribute("USER-ID", loginId);
+            filterChain.doFilter(request, response);
+        } else {
             checkRefreshToken(loginId, refreshToken);
             reIssueAccessTokenAndRefreshToken(response, loginId);
-            return;
         }
 
-        // RefreshToken이 없거나 유효하지 않다면 AccessToken을 검사하고 인증 처리
-        // AccessToken이 없거나 유효하지 않다면, 인증 객체가 담기지 않은 상태로 다음 필터로 넘어가기 때문에 403 에러 발생
-        // AccessToken이 유효하다면, 인증 객체가 담긴 상태로 다음 필터로 넘어가기 때문에 인증 성공
-        saveAuthentication(getUserWithAccessToken(accessToken));
-        request.setAttribute("USER-ID", loginId);
-        filterChain.doFilter(request, response);
+    }
+
+    private boolean tokenAuthenticationNotRequired(HttpServletRequest request) {
+        return request.getRequestURI().contains(USER_SERVICE_URL) || request.getRequestURI().contains(LOGIN_URL) ||
+                request.getRequestURI().contains(OAUTH2_URL);
     }
 
     private void checkIfTokenIsLogout(String accessToken) {
-        if(accessToken != null && redisUtil.get(accessToken) != null) {
+        if (accessToken != null && redisUtil.get(accessToken) != null) {
             throw new JwtException(TOKEN_LOGOUT);
         }
     }
 
     private void checkRefreshToken(String loginId, String refreshToken) {
         String redisRefreshToken = (String) redisUtil.get(loginId);
-        if(redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
+        if (redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
             throw new JwtException(DIFFERENT_REFRESH_TOKEN);
         }
     }
 
-    // 새로운 RefreshToken을 발급하여 Redis에 저장하고, AccessToken과 함께 응답
     private void reIssueAccessTokenAndRefreshToken(HttpServletResponse response, String loginId) {
         String reIssuedRefreshToken = jwtService.createRefreshToken(LocalDateTime.now());
         String reIssuedAccessToken = jwtService.createAccessToken(loginId, LocalDateTime.now());
@@ -103,7 +101,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
                 .orElseThrow(() -> new JwtException(USER_NOT_FOUND_BY_LOGIN_ID));
     }
 
-    // 인증 객체 저장
     private void saveAuthentication(User user) {
         UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                 .username(user.getLoginId())
@@ -111,11 +108,9 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
                 .roles(user.getRole().name())
                 .build();
 
-        // 인증 객체 생성
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
                 authoritiesMapper.mapAuthorities(userDetails.getAuthorities()));
 
-        // SecurityContext에 인증 객체 저장
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
