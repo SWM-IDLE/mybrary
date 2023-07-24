cf7ce73950da79b209c5f14fe5aa40c099cacc20
0112leesy <0112leesy@naver.com>
2023-07-24 23:54:23 +0900

    [IDLE-62] refactor: 문자열 상수 처리
      - 코드 리뷰 반영 (r2: "logout"를 상수처리)

--- user-service/src/main/java/kr/mybrary/userservice/authentication/domain/logout/handler/CustomLogoutHandler.java
+++ user-service/src/main/java/kr/mybrary/userservice/authentication/domain/logout/handler/CustomLogoutHandler.java
@@ -19,6 +19,7 @@ public class CustomLogoutHandler implements LogoutHandler {
 
     private static final String ACCESS_TOKEN_NOT_FOUND = "로그아웃 처리할 액세스 토큰이 존재하지 않습니다.";
     private static final String LOGIN_ID_NOT_FOUND = "로그아웃 처리할 로그인 아이디가 존재하지 않습니다.";
+    private static final String LOGOUT_VALUE = "logout";
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
@@ -29,7 +30,7 @@ public class CustomLogoutHandler implements LogoutHandler {
         String loginId = jwtService.getLoginId(accessToken).orElseThrow(() -> new JwtException(LOGIN_ID_NOT_FOUND));
 
         // 액세스 토큰 블랙리스트 등록
-        redisUtil.setBlackList(accessToken, "logout", jwtService.getExpirationDuration(accessToken, LocalDateTime.now()));
+        redisUtil.setBlackList(accessToken, LOGOUT_VALUE, jwtService.getExpirationDuration(accessToken, LocalDateTime.now()));
 
         // 로그인 아이디로 저장된 리프레쉬 토큰 삭제
         redisUtil.delete(loginId);
