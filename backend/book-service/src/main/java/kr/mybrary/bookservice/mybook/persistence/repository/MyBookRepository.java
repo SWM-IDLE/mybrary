658d9b4507c52b25e2da0a8a792794273e97c091
minseong <kdsvip5@naver.com>
2023-07-24 16:48:42 +0900

    [IDLE-133] perf: 마이북 상세 조회시, fetch join을 통해서 조회 - 4개의 select문(myBook, book, myBookMeaningTag, meaningTag)을 1개의 select문으로 개선 - myBookMeaning이 null일 경우가 존재하여 left fetch join 사용 - 테스트 코드 작성

--- backend/book-service/src/main/java/kr/mybrary/bookservice/mybook/persistence/repository/MyBookRepository.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/mybook/persistence/repository/MyBookRepository.java
@@ -1,6 +1,7 @@
 package kr.mybrary.bookservice.mybook.persistence.repository;
 
 import java.util.List;
+import java.util.Optional;
 import kr.mybrary.bookservice.book.persistence.Book;
 import kr.mybrary.bookservice.mybook.persistence.MyBook;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +17,11 @@ public interface MyBookRepository extends JpaRepository<MyBook, Long> {
             + "join fetch mbt.meaningTag "
             + "mt where mt.quote = :quote")
     List<MyBook> findByMeaningTagQuote(String quote);
+
+    @Query("select m from MyBook m "
+            + "left join fetch m.book "
+            + "left join fetch m.myBookMeaningTag mbt "
+            + "left join fetch mbt.meaningTag "
+            + "where m.id = :mybookId")
+    Optional<MyBook> findMyBookDetail(Long mybookId);
 }
