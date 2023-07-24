fe65502e344dece6c476d17249c1a7b653924d6b
0112leesy <0112leesy@naver.com>
2023-07-25 01:16:41 +0900

    [IDLE-139] feat: 사용자 검색 api 생성
      - 사용자 검색 api: GET api/v1/users/search/?nickname={검색할 닉네임}
      - 검색 결과 응답 dto: SearchServiceResponse
      - 검색 함수: searchByNickname(String nickname)

---
+++ user-service/src/main/java/kr/mybrary/userservice/user/domain/dto/response/SearchServiceResponse.java
@@ -0,0 +1,22 @@
+package kr.mybrary.userservice.user.domain.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class SearchServiceResponse {
+
+    private List<SearchedUser> searchedUsers;
+
+    @Getter
+    @Builder
+    public static class SearchedUser {
+        private String loginId;
+        private String nickname;
+        private String profileImageUrl;
+    }
+
+}
