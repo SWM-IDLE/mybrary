b44ac5104407122b4108397e69422f81dd11a29a
0112leesy <0112leesy@naver.com>
2023-07-25 13:28:32 +0900

    [IDLE-139] feat: MethodArgumentNotValidException 핸들러 생성
      - Validator에서 발생하는 예외 처리
      - BindingResult에 담긴 원인들을 모두 반환

--- user-service/src/main/java/kr/mybrary/userservice/global/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/kr/mybrary/userservice/global/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package kr.mybrary.userservice.global.exception;
 
 import kr.mybrary.userservice.global.dto.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +21,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> runtimeException(RuntimeException e) {
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of("RT-01", e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of("VLD-01", e.getBindingResult().getAllErrors().stream()
+                        .map(error -> error.getDefaultMessage())
+                        .collect(Collectors.joining(", "))));
     }
 }
