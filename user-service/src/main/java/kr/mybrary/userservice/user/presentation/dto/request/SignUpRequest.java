6c9d091da921dd400176bb9ab7e306e341012d7d
0112leesy <0112leesy@naver.com>
2023-07-25 13:31:20 +0900

    [IDLE-139] test: SignUpRequest, ProfileUpdateRequest ValidationTest
      - chore: SignUpRequest 정규 표현식 및 에러 메시지 수정
      - Validator 객체 선언 -> validator.validate(request)로 유효성 검증 테스트 수행

--- user-service/src/main/java/kr/mybrary/userservice/user/presentation/dto/request/SignUpRequest.java
+++ user-service/src/main/java/kr/mybrary/userservice/user/presentation/dto/request/SignUpRequest.java
@@ -17,16 +17,17 @@ public class SignUpRequest {
 
     @NotNull
     @NotBlank(message = "로그인 아이디는 필수입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9_-]{6,}$", message = "로그인 아이디는 6자 이상의 영문, 숫자 구성이어야 합니다. (하이픈과 언더바는 허용)")
     private String loginId;
 
     @NotNull
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}$", message = "비밀번호는 8~16자 영문, 숫자, 특수문자 구성이어야 합니다.")
     private String password;
 
     @NotNull
     @NotBlank(message = "닉네임은 필수입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,20}$", message = "닉네임은 특수문자를 제외한 2~20자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9_-]{2,20}$", message = "닉네임은 특수문자를 제외한 2~20자리여야 합니다.")
     private String nickname;
 
     @Email(message = "이메일 형식이 올바르지 않습니다.")
