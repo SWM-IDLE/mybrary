176d387755c91f46f5dd07e6a0e396e1b9504d73
minseong <kdsvip5@naver.com>
2023-07-25 17:34:49 +0900

    [IDLE-133] feat: 기존에 설정된 의미 태그를 변경시, 기존 의미 태그를 등록한 수를 -1 한다. - 테스트 코드 작성

--- backend/book-service/src/test/java/kr/mybrary/bookservice/tag/MeaningTagFixture.java
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/tag/MeaningTagFixture.java
@@ -7,7 +7,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum MeaningTagFixture {
 
-    COMMON_MEANING_TAG(1L, "의미있는 책", 1, "LOGIN_USER_ID");
+    COMMON_MEANING_TAG(1L, "의미있는 책", 1, "LOGIN_USER_ID"),
+    COMMON_MEANING_REGISTERED_COUNT_10_TAG(2L, "재미있는 책", 10, "LOGIN_USER_ID");
 
     private final Long id;
     private final String quote;
