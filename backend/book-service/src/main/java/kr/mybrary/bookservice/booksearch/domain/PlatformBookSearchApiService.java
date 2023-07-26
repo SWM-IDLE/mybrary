abde390f1ba04366d865be8ddefec366884705b8
minseong <kdsvip5@naver.com>
2023-07-26 16:58:51 +0900

    [IDLE-129] refactor: PlatformBookSearchApiService 내 추상 메서드 반환값을 Object로 변경 - 카카오 api와 알라딘 api의 응답값이 다르다. - 응답 클래스 이름 변경

--- backend/book-service/src/main/java/kr/mybrary/bookservice/booksearch/domain/PlatformBookSearchApiService.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/booksearch/domain/PlatformBookSearchApiService.java
@@ -1,10 +1,8 @@
 package kr.mybrary.bookservice.booksearch.domain;
 
-import kr.mybrary.bookservice.booksearch.presentation.dto.response.BookSearchResultResponse;
-
 public interface PlatformBookSearchApiService {
 
-    BookSearchResultResponse searchWithKeyword(Object request);
+    Object searchWithKeyword(Object request);
 
-    BookSearchResultResponse searchWithISBN(Object request);
+    Object searchWithISBN(Object request);
 }
