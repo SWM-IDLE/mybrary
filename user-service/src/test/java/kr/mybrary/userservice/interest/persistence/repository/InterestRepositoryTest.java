f3013ca94f20e06813c4d3a10743805e37cb99a0
0112leesy <0112leesy@naver.com>
2023-07-27 20:14:42 +0900

    [IDLE-140] test: InterestRepositoryTest 테스트 케이스 추가
      - findAllByCategory 테스트
      - 관심사에 카테고리가 추가될 때, 카테고리가 영속화되도록 설정

--- user-service/src/test/java/kr/mybrary/userservice/interest/persistence/repository/InterestRepositoryTest.java
+++ user-service/src/test/java/kr/mybrary/userservice/interest/persistence/repository/InterestRepositoryTest.java
@@ -1,6 +1,7 @@
 package kr.mybrary.userservice.interest.persistence.repository;
 
 import kr.mybrary.userservice.interest.persistence.Interest;
+import kr.mybrary.userservice.interest.persistence.InterestCategory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -31,9 +35,37 @@ class InterestRepositoryTest {
 
         // then
         assertAll(
-                () -> assertNotNull(foundInterest),
-                () -> assertEquals(savedInterest.getId(), foundInterest.getId()),
-                () -> assertEquals(savedInterest.getName(), foundInterest.getName())
+                () -> assertThat(foundInterest).isNotNull(),
+                () -> assertThat(foundInterest.getId()).isEqualTo(savedInterest.getId()),
+                () -> assertThat(foundInterest.getName()).isEqualTo(savedInterest.getName())
+        );
+    }
+
+    @Test
+    @DisplayName("카테고리에 해당하는 관심사를 가져온다.")
+    void findAllByCategory() {
+        // given
+        InterestCategory interestCategory = InterestCategory.builder()
+                .name("테스트 카테고리")
+                .description("테스트 카테고리 설명")
+                .build();
+        Interest savedInterest = interestRepository.save(Interest.builder()
+                .name("테스트 관심사")
+                .category(interestCategory)
+                .build());
+        Interest savedInterest2 = interestRepository.save(Interest.builder()
+                .name("테스트 관심사2")
+                .category(interestCategory)
+                .build());
+
+        // when
+        List<Interest> foundInterest = interestRepository.findAllByCategory(interestCategory);
+
+        // then
+        assertAll(
+                () -> assertThat(foundInterest).isNotEmpty(),
+                () -> assertThat(foundInterest).hasSize(2),
+                () -> assertThat(foundInterest).contains(savedInterest, savedInterest2)
         );
     }
 
