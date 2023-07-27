f2c6c5c3b7c068e75a296433da04e2496922c34b
0112leesy <0112leesy@naver.com>
2023-07-27 20:00:04 +0900

    [IDLE-140] feat: 카테고리별 관심사 목록 조회 기능 개발
      - getInterestsGroupByCategory 구현
      - InterestCategoryServiceResponse DTO 생성

---
+++ user-service/src/main/java/kr/mybrary/userservice/interest/domain/InterestService.java
@@ -0,0 +1,9 @@
+package kr.mybrary.userservice.interest.domain;
+
+import kr.mybrary.userservice.interest.domain.dto.response.InterestCategoryServiceResponse;
+
+public interface InterestService {
+
+    InterestCategoryServiceResponse getInterestsGroupByCategory();
+
+}
--- user-service/src/main/java/kr/mybrary/userservice/interest/persistence/repository/InterestRepository.java
+++ user-service/src/main/java/kr/mybrary/userservice/interest/persistence/repository/InterestRepository.java
@@ -1,6 +1,7 @@
 package kr.mybrary.userservice.interest.persistence.repository;
 
 import kr.mybrary.userservice.interest.persistence.Interest;
+import kr.mybrary.userservice.interest.persistence.InterestCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,8 +9,7 @@ import java.util.Optional;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
-    List<Interest> findAll();
-
     Optional<Interest> findByName(String name);
+    List<Interest> findAllByCategory(InterestCategory category);
 
 }
