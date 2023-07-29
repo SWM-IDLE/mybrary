233735d02f7c005d49f67ce7b77caa727b6f6565
0112leesy <0112leesy@naver.com>
2023-07-29 18:51:35 +0900

    [IDLE-141] feat: 회원 가입 시 한 줄 소개 공백 처리
      - 기존) 회원 가입 시 한 줄 소개: null
      - 변경) 회원 가입 시 한 줄 소개: ""(빈 문자열)

--- user-service/src/main/java/kr/mybrary/userservice/user/domain/dto/UserMapper.java
+++ user-service/src/main/java/kr/mybrary/userservice/user/domain/dto/UserMapper.java
@@ -6,6 +6,7 @@ import kr.mybrary.userservice.user.domain.dto.response.SearchServiceResponse;
 import kr.mybrary.userservice.user.domain.dto.response.SignUpServiceResponse;
 import kr.mybrary.userservice.user.persistence.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -14,6 +15,7 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "introduction", expression = "java(\"\")")
     User toEntity(SignUpServiceRequest serviceRequest);
 
     SignUpServiceResponse toSignUpServiceResponse(User user);
