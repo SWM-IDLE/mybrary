82e76718f567c5c2eb451d38c3727518b20e2783
0112leesy <0112leesy@naver.com>
2023-07-30 21:42:36 +0900

    [IDLE-141] feat: 사용자 관심사 조회 api
      - 사용자 관심사 조회: GET /api/v1/users/{userId}/interests

--- user-service/src/main/java/kr/mybrary/userservice/interest/presentation/InterestController.java
+++ user-service/src/main/java/kr/mybrary/userservice/interest/presentation/InterestController.java
@@ -5,9 +5,7 @@ import kr.mybrary.userservice.interest.domain.InterestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,13 @@ public class InterestController {
     public ResponseEntity<SuccessResponse> getInterestCategories() {
         return ResponseEntity.ok().body(
                 SuccessResponse.of(HttpStatus.OK.toString(), "카테고리별 관심사를 모두 조회했습니다.", interestService.getInterestCategories())
+        );
+    }
+
+    @GetMapping("/users/{userId}/interests")
+    public ResponseEntity<SuccessResponse> getUserInterests(@PathVariable("userId") String loginId) {
+        return ResponseEntity.ok().body(
+                SuccessResponse.of(HttpStatus.OK.toString(), "사용자의 관심사를 모두 조회했습니다.", interestService.getUserInterests(loginId))
         );
     }
 
