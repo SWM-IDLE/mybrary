f2723cd3bc0529093ed950aabbee59e30d1f7a5b
0112leesy <0112leesy@naver.com>
2023-07-30 20:31:03 +0900

    [IDLE-141] test: UserInterestRepository 테스트
      - 페치 조인을 사용하여 해당 사용자의 관심사를 모두 가져오는지 확인
      - test: InterestCategoryRepositoryTest에 프록시 객체 확인 추가

---
+++ user-service/src/test/java/kr/mybrary/userservice/interest/persistence/repository/UserInterestRepositoryTest.java
@@ -0,0 +1,74 @@
+package kr.mybrary.userservice.interest.persistence.repository;
+
+import kr.mybrary.userservice.interest.InterestFixture;
+import kr.mybrary.userservice.interest.persistence.Interest;
+import kr.mybrary.userservice.interest.persistence.UserInterest;
+import kr.mybrary.userservice.user.UserFixture;
+import kr.mybrary.userservice.user.persistence.User;
+import kr.mybrary.userservice.user.persistence.repository.UserRepository;
+import org.hibernate.proxy.HibernateProxy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@DataJpaTest
+class UserInterestRepositoryTest {
+
+    @Autowired
+    private UserInterestRepository userInterestRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private InterestRepository interestRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    @DisplayName("페치 조인을 사용하여 해당 사용자의 관심사를 모두 가져온다.")
+    void findAllByUserWithInterestUsingFetchJoin() {
+        // given
+        User user = UserFixture.COMMON_USER.getUser();
+        Interest interest1 = InterestFixture.DOMESTIC_NOVEL.getInterest();
+        Interest interest2 = InterestFixture.FOREIGN_NOVEL.getInterest();
+
+        userRepository.save(user);
+        interestRepository.save(interest1);
+        interestRepository.save(interest2);
+
+        userInterestRepository.save(UserInterest.builder()
+                .user(user)
+                .interest(interest1)
+                .build());
+        userInterestRepository.save(UserInterest.builder()
+                .user(user)
+                .interest(interest2)
+                .build());
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        List<UserInterest> foundUserInterests = userInterestRepository.findAllByUserWithInterestUsingFetchJoin(user);
+
+        // then
+        assertAll(
+                () -> assertThat(foundUserInterests).isNotNull(),
+                () -> assertThat(foundUserInterests.size()).isEqualTo(2),
+                () -> assertThat(foundUserInterests.get(0).getInterest().getName()).isEqualTo("국내소설"),
+                () -> assertThat(foundUserInterests.get(1).getInterest().getName()).isEqualTo("외국소설"),
+                () -> assertThat(foundUserInterests.get(0).getInterest()).isNotInstanceOf(HibernateProxy.class),
+                () -> assertThat(foundUserInterests.get(1).getInterest()).isNotInstanceOf(HibernateProxy.class)
+        );
+    }
+}
