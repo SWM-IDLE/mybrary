49b0418d92b807cabfdd01b7140cab5c9c014b4b
0112leesy <0112leesy@naver.com>
2023-07-31 13:48:23 +0900

    [IDLE-141] fix: findAllByUserWithInterestUsingFetchJoin 쿼리 수정
      - 문제) 특정 user의 관심사를 가져오지 않고 모든 사용자의 관심사를 가져옴
      - 해결) 쿼리에 WHERE ui.user = :user 를 추가해 특정 사용자의 관심사만 반환하도록 수정

--- backend/user-service/src/main/java/kr/mybrary/userservice/interest/persistence/repository/UserInterestRepository.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/interest/persistence/repository/UserInterestRepository.java
@@ -4,12 +4,13 @@ import kr.mybrary.userservice.interest.persistence.UserInterest;
 import kr.mybrary.userservice.user.persistence.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
 
-    @Query("SELECT distinct ui FROM UserInterest ui join fetch ui.interest")
-    List<UserInterest> findAllByUserWithInterestUsingFetchJoin(User user);
+    @Query("SELECT distinct ui FROM UserInterest ui join fetch ui.interest WHERE ui.user = :user")
+    List<UserInterest> findAllByUserWithInterestUsingFetchJoin(@Param("user") User user);
 
 }
