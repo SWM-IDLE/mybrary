41b6969277613e0c72293ca731cd6bd102a71a11
minseong <kdsvip5@naver.com>
2023-08-01 16:31:28 +0900

    [IDLE-169] feat: Book 조회를 위한 serviceRequest, serviceResponse 추가 - mapStruct에 mapping 메서드 추가 후 테스트 코드 작성

--- backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/dto/BookDtoMapper.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/book/domain/dto/BookDtoMapper.java
@@ -1,9 +1,14 @@
 package kr.mybrary.bookservice.book.domain.dto;
 
+import java.util.List;
 import kr.mybrary.bookservice.book.domain.dto.request.BookCreateServiceRequest;
+import kr.mybrary.bookservice.book.domain.dto.response.BookDetailServiceResponse;
 import kr.mybrary.bookservice.book.persistence.Book;
+import kr.mybrary.bookservice.book.persistence.author.BookAuthor;
+import kr.mybrary.bookservice.book.persistence.translator.BookTranslator;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
@@ -20,4 +25,33 @@ public interface BookDtoMapper {
     @Mapping(target = "aladinStarRating", source = "starRating")
     @Mapping(target = "aladinReviewCount", source = "reviewCount")
     Book bookCreateRequestToEntity(BookCreateServiceRequest request);
+
+
+    @Mapping(target = "starRating", source = "aladinStarRating")
+    @Mapping(target = "reviewCount", source = "aladinReviewCount")
+    @Mapping(target = "authors", source = "bookAuthors", qualifiedByName = "mappingAuthors")
+    @Mapping(target = "translators", source = "bookTranslators", qualifiedByName = "mappingTranslators")
+    BookDetailServiceResponse bookToDetailServiceResponse(Book book);
+
+    @Named("mappingAuthors")
+    static List<BookDetailServiceResponse.Author> mappingAuthors(List<BookAuthor> bookAuthors) {
+        return bookAuthors.stream()
+                .map(BookAuthor::getAuthor)
+                .map(author -> BookDetailServiceResponse.Author.builder()
+                            .name(author.getName())
+                            .authorId(author.getAid())
+                            .build()
+                ).toList();
+    }
+
+    @Named("mappingTranslators")
+    static List<BookDetailServiceResponse.Translator> mappingTranslators(List<BookTranslator> bookTranslators) {
+        return bookTranslators.stream()
+                .map(BookTranslator::getTranslator)
+                .map(translator -> BookDetailServiceResponse.Translator.builder()
+                        .name(translator.getName())
+                        .translatorId(translator.getTid())
+                        .build()
+                ).toList();
+    }
 }
