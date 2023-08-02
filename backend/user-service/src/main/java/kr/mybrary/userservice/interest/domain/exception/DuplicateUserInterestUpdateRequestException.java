9e31fb81b051e726fbf14dc63d2a163aa39f3a04
0112leesy <0112leesy@naver.com>
2023-08-02 17:21:57 +0900

    [IDLE-195] test: 사용자 관심사 등록 기능 테스트
      - 사용자의 관심사를 업데이트한다.
      - 사용자가 존재하지 않으면 예외가 발생한다.
      - 관심사가 존재하지 않으면 예외가 발생한다.
      - 관심사가 3개보다 많으면 예외가 발생한다.
      - 중복된 관심사가 있으면 예외가 발생한다.
    
      - refactor: deleteOriginalUserInterests 함수화
      - chore: 예외 메시지 수정

--- backend/user-service/src/main/java/kr/mybrary/userservice/interest/domain/exception/DuplicateUserInterestUpdateRequestException.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/interest/domain/exception/DuplicateUserInterestUpdateRequestException.java
@@ -6,7 +6,7 @@ public class DuplicateUserInterestUpdateRequestException extends ApplicationExce
 
     private final static int STATUS = 400;
     private final static String ERROR_CODE = "I-03";
-    private final static String ERROR_MESSAGE = "관심사는 중복해서 선택할 수 없습니다.";
+    private final static String ERROR_MESSAGE = "관심사는 중복해서 설정할 수 없습니다.";
 
     public DuplicateUserInterestUpdateRequestException() {
         super(STATUS, ERROR_CODE, ERROR_MESSAGE);
--- backend/user-service/src/main/java/kr/mybrary/userservice/interest/domain/exception/UserInterestUpdateRequestSizeExceededException.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/interest/domain/exception/UserInterestUpdateRequestSizeExceededException.java
@@ -6,7 +6,7 @@ public class UserInterestUpdateRequestSizeExceededException extends ApplicationE
 
     private final static int STATUS = 400;
     private final static String ERROR_CODE = "I-02";
-    private final static String ERROR_MESSAGE = "관심사는 최대 3개까지 선택할 수 있습니다.";
+    private final static String ERROR_MESSAGE = "관심사는 최대 3개까지 설정할 수 있습니다.";
 
     public UserInterestUpdateRequestSizeExceededException() {
         super(STATUS, ERROR_CODE, ERROR_MESSAGE);
