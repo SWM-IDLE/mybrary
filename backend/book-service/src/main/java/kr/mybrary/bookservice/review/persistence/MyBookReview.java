f4d30b0f52ec9b1507e85b0706454ee40e85a77e
minseong <kdsvip5@naver.com>
2023-08-06 23:50:57 +0900

    [IDLE-134] feat: 마이리뷰 엔티티 생성

---
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/MyBookReview.java
@@ -0,0 +1,45 @@
+package kr.mybrary.bookservice.review.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import kr.mybrary.bookservice.book.persistence.Book;
+import kr.mybrary.bookservice.global.BaseEntity;
+import kr.mybrary.bookservice.mybook.persistence.MyBook;
+import kr.mybrary.bookservice.review.domain.dto.request.MyBookReviewCreateServiceRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "deleted = false")
+public class MyBookReview extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private MyBook myBook;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Book book;
+
+    @Column(length = 500)
+    private String content;
+
+    private Double starRating;
+    private boolean deleted;
+
+}
