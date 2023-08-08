9087ada408ae07cdc60f5b8252df01c23321f4ff
minseong <kdsvip5@naver.com>
2023-08-08 14:44:28 +0900

    [IDLE-196] feat: book-service AOP 적용 및 모든 Request 마다 로깅 처리 - logging level - info - 로깅 예시 : Request: GET /api/v1/books/detail [isbn13 -> (9791167224606)] < 0:0:0:0:0:0:0:1 (181ms)

--- backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy
 public class BookServiceApplication {
 
     public static void main(String[] args) {
