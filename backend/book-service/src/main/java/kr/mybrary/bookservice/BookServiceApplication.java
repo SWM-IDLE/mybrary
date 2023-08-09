013150e52b808b489509c034b7699fb1859c88a8
minseong <kdsvip5@naver.com>
2023-08-09 17:18:11 +0900

    [IDLE-134] openfeign 의존성 추가 - @EnableFeignClients 메인 클래스에 추가

--- backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/BookServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy
+@EnableFeignClients
 public class BookServiceApplication {
 
     public static void main(String[] args) {
