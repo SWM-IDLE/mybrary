c382eba767d871a9e35a54c9105109ed572022f2
minseong <kdsvip5@naver.com>
2023-08-09 17:50:27 +0900

    [IDLE-134] 도서의 리뷰 조회 서비스 계층 코드 작성 - UserServiceClient 사용

--- backend/book-service/src/test/java/kr/mybrary/bookservice/review/MyBookReviewDtoTestData.java
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/review/MyBookReviewDtoTestData.java
@@ -1,6 +1,12 @@
 package kr.mybrary.bookservice.review;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import kr.mybrary.bookservice.client.user.dto.response.UserInfoServiceResponse;
+import kr.mybrary.bookservice.client.user.dto.response.UserInfoServiceResponse.UserInfoElement;
 import kr.mybrary.bookservice.review.domain.dto.request.MyBookReviewCreateServiceRequest;
+import kr.mybrary.bookservice.review.domain.dto.request.ReviewsOfBookGetServiceRequest;
+import kr.mybrary.bookservice.review.persistence.dto.MyBookReviewElementDto;
 import kr.mybrary.bookservice.review.presentation.dto.request.MyBookReviewCreateRequest;
 
 public class MyBookReviewDtoTestData {
@@ -18,6 +24,39 @@ public class MyBookReviewDtoTestData {
         return MyBookReviewCreateRequest.builder()
                 .content("리뷰 내용입니다.")
                 .starRating(4.5)
+                .build();
+    }
+
+    public static ReviewOfBookGetServiceRequestBuilder createReviewOfBookGetServiceRequest() {
+        return ReviewsOfBookGetServiceRequest.builder()
+                .isbn13("ISBN13");
+    }
+
+    public static List<MyBookReviewElementDto> createMyBookReviewElementDtoList() {
+
+        return IntStream.range(1, 6)
+                .mapToObj(i -> MyBookReviewElementDto.builder()
+                        .id((long) i)
+                        .userId("USER_ID_" + i)
+                        .content("리뷰_내용_" + i)
+                        .starRating((double) i)
+                        .createdAt(null)
+                        .build())
+                .toList();
+    }
+
+    public static UserInfoServiceResponse createUserInfoResponseList() {
+
+        List<UserInfoElement> list = IntStream.range(1, 6)
+                .mapToObj(i -> UserInfoElement.builder()
+                        .userId("USER_ID_" + i)
+                        .nickname("USER_NICKNAME_" + i)
+                        .profileImageUrl("USER_PICTURE_URL_" + i)
+                        .build())
+                .toList();
+
+        return UserInfoServiceResponse.builder()
+                .userInfoElements(list)
                 .build();
     }
 }
