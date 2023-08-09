c9a2f13ac6c258510b1a44af0faa2f78faa2614d
0112leesy <0112leesy@naver.com>
2023-08-09 20:55:18 +0900

    [IDLE-173] feat: UserRepositoryCustom 생성 및 구현
      - 필요한 칼럼만 가져오기 위한 UserInfoModel 생성
      - findAllUserInfoByLoginIds 구현

--- backend/user-service/src/main/java/kr/mybrary/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/UserServiceApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableJpaAuditing
 @EnableDiscoveryClient
 @EnableAspectJAutoProxy
 public class UserServiceApplication {
