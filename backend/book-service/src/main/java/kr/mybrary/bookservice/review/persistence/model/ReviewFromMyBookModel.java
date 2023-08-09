ea8ae8cb11159b7a9733d2e330dbda013ab01815
minseong <kdsvip5@naver.com>
2023-08-10 02:18:23 +0900

    [IDLE-134] 마이북을 통해 조회하는 마이 리뷰 Model 생성 및 query dsl 조회 메서드 추가 - 마이북 상세 페이지에 필요한 마이 리뷰 response에 맞도록 필요한 데이터만 조회 - 테스트 코드 작성

---
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/model/ReviewFromMyBookModel.java
@@ -0,0 +1,20 @@
+package kr.mybrary.bookservice.review.persistence.model;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewFromMyBookModel {
+
+    private Long id;
+    private String content;
+    private Double starRating;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
--- backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/repository/MyBookReviewRepositoryImpl.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/repository/MyBookReviewRepositoryImpl.java
@@ -5,8 +5,11 @@ import static kr.mybrary.bookservice.review.persistence.QMyBookReview.myBookRevi
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.Optional;
 import kr.mybrary.bookservice.book.persistence.Book;
-import kr.mybrary.bookservice.review.persistence.dto.MyBookReviewElementDto;
+import kr.mybrary.bookservice.mybook.persistence.MyBook;
+import kr.mybrary.bookservice.review.persistence.model.MyBookReviewElementDto;
+import kr.mybrary.bookservice.review.persistence.model.ReviewFromMyBookModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -27,5 +30,22 @@ public class MyBookReviewRepositoryImpl implements MyBookReviewRepositoryCustom 
                         myBookReview.createdAt.as("createdAt")))
                 .from(myBookReview)
                 .fetch();
+    }
+
+    @Override
+    public Optional<ReviewFromMyBookModel> findReviewByMyBook(MyBook myBook) {
+
+        ReviewFromMyBookModel reviewFromMyBookModel = queryFactory
+                .select(Projections.fields(ReviewFromMyBookModel.class,
+                        myBookReview.id.as("id"),
+                        myBookReview.content.as("content"),
+                        myBookReview.starRating.as("starRating"),
+                        myBookReview.createdAt.as("createdAt"),
+                        myBookReview.updatedAt.as("updatedAt")))
+                .from(myBookReview)
+                .where(myBookReview.myBook.eq(myBook))
+                .fetchOne();
+
+        return Optional.ofNullable(reviewFromMyBookModel);
     }
 }
