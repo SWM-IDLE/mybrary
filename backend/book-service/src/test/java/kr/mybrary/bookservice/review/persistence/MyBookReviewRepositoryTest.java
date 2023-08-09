a7aec5f32fa76f73b8b021bd0bb5ded8e78d9e16
minseong <kdsvip5@naver.com>
2023-08-10 02:18:23 +0900

    [IDLE-134] 마이북을 통해 조회하는 마이 리뷰 Model 생성 및 query dsl 조회 메서드 추가 - 마이북 상세 페이지에 필요한 마이 리뷰 response에 맞도록 필요한 데이터만 조회 - 테스트 코드 작성

--- backend/book-service/src/test/java/kr/mybrary/bookservice/review/persistence/MyBookReviewRepositoryTest.java
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/review/persistence/MyBookReviewRepositoryTest.java
@@ -4,13 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import java.util.Optional;
 import kr.mybrary.bookservice.PersistenceTest;
 import kr.mybrary.bookservice.book.BookFixture;
 import kr.mybrary.bookservice.book.persistence.Book;
 import kr.mybrary.bookservice.mybook.MyBookFixture;
 import kr.mybrary.bookservice.mybook.persistence.MyBook;
 import kr.mybrary.bookservice.review.MyBookReviewFixture;
-import kr.mybrary.bookservice.review.persistence.dto.MyBookReviewElementDto;
+import kr.mybrary.bookservice.review.persistence.model.MyBookReviewElementDto;
+import kr.mybrary.bookservice.review.persistence.model.ReviewFromMyBookModel;
 import kr.mybrary.bookservice.review.persistence.repository.MyBookReviewRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -78,6 +80,37 @@ class MyBookReviewRepositoryTest {
                     assertThat(reviewsByBook.get(0).getContent()).isEqualTo(myBookReview.getContent());
                     assertThat(reviewsByBook.get(0).getCreatedAt()).isEqualTo(myBookReview.getCreatedAt());
                     assertThat(reviewsByBook.get(0).getStarRating()).isEqualTo(myBookReview.getStarRating());
+                }
+        );
+    }
+
+    @DisplayName("마이북을 통해 마이북 리뷰를 조회한다.")
+    @Test
+    void findReviewByMyBook() {
+
+        // given
+        Book book = entityManager.persist(BookFixture.COMMON_BOOK_WITHOUT_RELATION.getBook());
+        MyBook myBook = entityManager.persist(
+                MyBookFixture.MY_BOOK_WITHOUT_RELATION.getMyBookBuilder().book(book).build());
+
+        MyBookReview myBookReview = entityManager.persist(MyBookReviewFixture.MY_BOOK_REVIEW_WITHOUT_RELATION
+                .getMyBookReviewBuilder().book(book).myBook(myBook).build());
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        Optional<ReviewFromMyBookModel> reviewByMyBook = myBookReviewRepository.findReviewByMyBook(myBook);
+
+        // then
+        assertAll(
+                () -> {
+                    assertThat(reviewByMyBook.isPresent()).isTrue();
+                    assertThat(reviewByMyBook.get().getId()).isEqualTo(myBookReview.getId());
+                    assertThat(reviewByMyBook.get().getContent()).isEqualTo(myBookReview.getContent());
+                    assertThat(reviewByMyBook.get().getStarRating()).isEqualTo(myBookReview.getStarRating());
+                    assertThat(reviewByMyBook.get().getCreatedAt()).isEqualTo(myBookReview.getCreatedAt());
+                    assertThat(reviewByMyBook.get().getUpdatedAt()).isEqualTo(myBookReview.getUpdatedAt());
                 }
         );
     }
