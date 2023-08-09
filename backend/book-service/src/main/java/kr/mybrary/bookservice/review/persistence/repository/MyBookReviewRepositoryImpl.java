f4a6f85da0b1e1bbe032e399a11edf5b6decc799
minseong <kdsvip5@naver.com>
2023-08-09 14:53:25 +0900

    [IDLE-134] MyBookReview QueryDsl 적용 및 findReviewsByBook 메서드 구현 - findReviewsByBook 사용 필요한 칼럼만 조회 (Projections.fields 사용) - 테스트 코드 작성

---
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/repository/MyBookReviewRepositoryImpl.java
@@ -0,0 +1,29 @@
+package kr.mybrary.bookservice.review.persistence.repository;
+
+import static kr.mybrary.bookservice.review.persistence.QMyBookReview.myBookReview;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import kr.mybrary.bookservice.book.persistence.Book;
+import kr.mybrary.bookservice.review.persistence.dto.MyBookReviewElementDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MyBookReviewRepositoryImpl implements MyBookReviewRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<MyBookReviewElementDto> findReviewsByBook(Book book) {
+        return queryFactory
+                .select(Projections.fields(MyBookReviewElementDto.class,
+                        myBookReview.myBook.userId.as("userId"),
+                        myBookReview.content.as("content"),
+                        myBookReview.createdAt.as("createdAt")))
+                .from(myBookReview)
+                .fetch();
+    }
+}
