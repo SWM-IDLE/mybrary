b42bad442eed5d2b7fa9bde36d83b4f49c8b5608
DonggyuJin <jdk2531@gmail.com>
2023-08-10 16:50:02 +0900

    [IDLE-179] chore: 마이북 관련 api path 설정 및 이전 path 수정 - 사용하지 않는 api path 삭제 - 마이북 및 리뷰 관련 api path 추가 - 호출되는 기능별 api path 네이밍을 create/get/update/delete 로 통일

--- frontend/lib/data/datasource/book/book_datasource.dart
+++ frontend/lib/data/datasource/book/book_datasource.dart
@@ -6,24 +6,24 @@ import 'package:mybrary/data/network/api.dart';
 import 'package:mybrary/utils/dios/dio_service.dart';
 
 class BookDataSource {
-  Future<InterestBookResponseData> registerInterestBook(
+  Future<InterestBookResponseData> createOrDeleteInterestBook(
     String userId,
     String isbn13,
   ) async {
     Dio dio = DioService().to();
-    final registerInterestBookResponse = await dio.post(
-      '${getBookServiceApi(API.registerInterestBook)}/$isbn13/interest',
+    final createOrDeleteInterestBookResponse = await dio.post(
+      '${getBookServiceApi(API.createOrDeleteInterestBook)}/$isbn13/interest',
       options: Options(headers: {'User-Id': userId}),
     );
 
-    log('관심 도서 등록 및 삭제 응답값: $registerInterestBookResponse');
+    log('관심 도서 등록 및 삭제 응답값: $createOrDeleteInterestBookResponse');
     final InterestBookResponse result = commonResponseResult(
-      registerInterestBookResponse,
+      createOrDeleteInterestBookResponse,
       () => InterestBookResponse(
-        status: registerInterestBookResponse.data['status'],
-        message: registerInterestBookResponse.data['message'],
+        status: createOrDeleteInterestBookResponse.data['status'],
+        message: createOrDeleteInterestBookResponse.data['message'],
         data: InterestBookResponseData.fromJson(
-          registerInterestBookResponse.data['data'],
+          createOrDeleteInterestBookResponse.data['data'],
         ),
       ),
     );
