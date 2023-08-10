3260b747d8a67a3ecacd191e1d3b679cca48dd0d
minseong <kdsvip5@naver.com>
2023-08-10 22:09:57 +0900

    [IDLE-135] feat: 마이 리뷰 조회시 마이북도 함께 fetch join 하는 메서드 작성 - 마이 리뷰를 작성한 사용자의 정보를 알기 위해서 마이북을 참조해야하는 상황에서 fetch join 사용

--- backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/repository/MyReviewRepository.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/review/persistence/repository/MyReviewRepository.java
@@ -1,10 +1,17 @@
 package kr.mybrary.bookservice.review.persistence.repository;
 
+import java.util.Optional;
 import kr.mybrary.bookservice.mybook.persistence.MyBook;
 import kr.mybrary.bookservice.review.persistence.MyReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MyReviewRepository extends JpaRepository<MyReview, Long>, MyReviewRepositoryCustom {
 
     boolean existsByMyBook(MyBook myBook);
+
+    @Query("select mr from MyReview mr "
+            + "join fetch mr.myBook mb "
+            + "where mr.id = :id")
+    Optional<MyReview> findByIdWithMyBookUsingFetchJoin(Long id);
 }
