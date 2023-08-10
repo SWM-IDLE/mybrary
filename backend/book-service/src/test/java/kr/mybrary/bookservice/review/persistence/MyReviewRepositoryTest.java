e3336c80bca6acd6f5e01c24d0f93ada39cd622c
minseong <kdsvip5@naver.com>
2023-08-10 22:09:57 +0900

    [IDLE-135] feat: 마이 리뷰 조회시 마이북도 함께 fetch join 하는 메서드 작성 - 마이 리뷰를 작성한 사용자의 정보를 알기 위해서 마이북을 참조해야하는 상황에서 fetch join 사용

--- backend/book-service/src/test/java/kr/mybrary/bookservice/review/persistence/MyReviewRepositoryTest.java
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/review/persistence/MyReviewRepositoryTest.java
@@ -11,9 +11,10 @@ import kr.mybrary.bookservice.book.persistence.Book;
 import kr.mybrary.bookservice.mybook.MyBookFixture;
 import kr.mybrary.bookservice.mybook.persistence.MyBook;
 import kr.mybrary.bookservice.review.MyBookReviewFixture;
-import kr.mybrary.bookservice.review.persistence.model.MyReviewFromMyBookModel;
 import kr.mybrary.bookservice.review.persistence.model.MyReviewElementModel;
+import kr.mybrary.bookservice.review.persistence.model.MyReviewFromMyBookModel;
 import kr.mybrary.bookservice.review.persistence.repository.MyReviewRepository;
+import org.hibernate.proxy.HibernateProxy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -113,5 +114,34 @@ class MyReviewRepositoryTest {
                     assertThat(reviewByMyBook.get().getUpdatedAt()).isNotNull();
                 }
         );
+    }
+
+    @DisplayName("마이 리뷰 조회시 연관되어 있는 마이북을 함께 조회한다.")
+    @Test
+    void findByIdWithMyBookUsingFetchJoin() {
+
+        // given
+        Book book = entityManager.persist(BookFixture.COMMON_BOOK_WITHOUT_RELATION.getBook());
+        MyBook myBook = entityManager.persist(
+                MyBookFixture.MY_BOOK_WITHOUT_RELATION.getMyBookBuilder().book(book).build());
+
+        MyReview myReview = entityManager.persist(MyBookReviewFixture.MY_BOOK_REVIEW_WITHOUT_RELATION
+                .getMyBookReviewBuilder().book(book).myBook(myBook).build());
+
+        entityManager.flush();
+        entityManager.clear();
+
+        // when
+        myBookReviewRepository.findByIdWithMyBookUsingFetchJoin(myReview.getId()).ifPresent(review -> {
+
+            // then
+            assertAll(
+                    () -> assertThat(review.getId()).isEqualTo(myReview.getId()),
+                    () -> assertThat(review.getContent()).isEqualTo(myReview.getContent()),
+                    () -> assertThat(review.getStarRating()).isEqualTo(myReview.getStarRating()),
+                    () -> assertThat(review.getMyBook()).isNotInstanceOf(HibernateProxy.class)
+            );
+        });
+
     }
 }
