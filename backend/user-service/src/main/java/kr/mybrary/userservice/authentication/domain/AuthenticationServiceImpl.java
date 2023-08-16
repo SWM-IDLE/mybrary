b1ed7d5f282bce6b5dc120b8c3e36243b1308ea2
Seyoung Lee <0112leesy@naver.com>
2023-08-17 01:33:16 +0900

    [IDLE-174] feat: user-service 토큰 재발급 시 예외 클래스 정의
      - 액세스 토큰이 없을 때 : AccessTokenNotFoundException
      - 리프레쉬 토큰이 없을 때 : RefreshTokenNotFoundException
      - 유효하지 않은 리프레쉬 토큰일 때 : InvalidRefreshTokenException

--- backend/user-service/src/main/java/kr/mybrary/userservice/authentication/domain/AuthenticationServiceImpl.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/authentication/domain/AuthenticationServiceImpl.java
@@ -2,6 +2,9 @@ package kr.mybrary.userservice.authentication.domain;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.mybrary.userservice.authentication.domain.exception.AccessTokenNotFoundException;
+import kr.mybrary.userservice.authentication.domain.exception.InvalidRefreshTokenException;
+import kr.mybrary.userservice.authentication.domain.exception.RefreshTokenNotFoundException;
 import kr.mybrary.userservice.global.util.JwtUtil;
 import kr.mybrary.userservice.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +23,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final JwtUtil jwtUtil;
     private final RedisUtil redisUtil;
 
-    private static final String DIFFERENT_REFRESH_TOKEN = "저장된 리프레쉬 토큰과 다릅니다.";
     private static final int REFRESH_TOKEN_EXPIRATION = 14;
 
     @Override
     public void reIssueToken(HttpServletRequest request, HttpServletResponse response) {
-        String accessToken = jwtUtil.extractAccessToken(request).orElseThrow(IllegalArgumentException::new);
-        String refreshToken = jwtUtil.extractRefreshToken(request).orElseThrow(IllegalArgumentException::new);
+        String accessToken = jwtUtil.extractAccessToken(request).orElseThrow(AccessTokenNotFoundException::new);
+        String refreshToken = jwtUtil.extractRefreshToken(request).orElseThrow(RefreshTokenNotFoundException::new);
 
         checkRefreshToken(jwtUtil.parseLoginId(accessToken), refreshToken);
         reIssueAccessTokenAndRefreshToken(response, jwtUtil.parseLoginId(accessToken));
@@ -35,7 +37,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private void checkRefreshToken(String loginId, String refreshToken) {
         String redisRefreshToken = (String) redisUtil.get(loginId);
         if (redisRefreshToken == null || !redisRefreshToken.equals(refreshToken)) {
-            throw new JwtException(DIFFERENT_REFRESH_TOKEN);
+            throw new InvalidRefreshTokenException();
         }
     }
 
