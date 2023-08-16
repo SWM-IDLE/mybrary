0a742b169267f52c7fd207547dfee647ef678910
minseong <kdsvip5@naver.com>
2023-08-16 21:39:00 +0900

    [IDLE-224] bug: feignClient 버그 해결 - GetMapping인 경우, ReqeustBody를 담아 요청을 보낼 수 없다, PostMapping으로 수정 - Post "/api/v1/users/info"의 경우 응답값이 status, message, data 형식으로 넘어오기 때문에, 응답객체의 형식을 맞춰야한다. - @Headers("Content-Type: application/json") 추가

--- backend/book-service/src/main/java/kr/mybrary/bookservice/client/user/dto/response/UserInfoServiceResponse.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/client/user/dto/response/UserInfoServiceResponse.java
@@ -1,18 +1,36 @@
 package kr.mybrary.bookservice.client.user.dto.response;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfoServiceResponse {
 
-    private List<UserInfoElement> userInfoElements;
+    private String status;
+    private String message;
+    private UserInfoList data;
 
     @Builder
     @Getter
-    public static class UserInfoElement {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserInfoList {
+
+        List<UserInfo> userInfoElements;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserInfo {
+
         private String userId;
         private String nickname;
         private String profileImageUrl;
