5cf88f07a018b4d5822b7ccbcf5bf9cf38e2f7ab
minseong <kdsvip5@naver.com>
2023-08-18 14:48:57 +0900

    [IDLE-202] test: yml 파일 수정 및 @SpringBootTest 제거 - 통합 테스트 경우 추후에 TestContainer를 추가하여 할 예정

--- backend/book-service/src/test/java/kr/mybrary/bookservice/BookServiceApplicationTests.java
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/BookServiceApplicationTests.java
@@ -1,11 +1,9 @@
 package kr.mybrary.bookservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@SpringBootTest
 class BookServiceApplicationTests {
 
 	@Test
