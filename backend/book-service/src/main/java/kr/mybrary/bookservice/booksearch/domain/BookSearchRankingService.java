a86f5a27cc3c3a4785ff4a4d95d437ff94a7a780
minseong <kdsvip5@naver.com>
2023-08-18 13:57:56 +0900

    [IDLE-202] feat: 도서 인기 검색어 조회 로직 구현 - 인기 검색어는 최대 10개까지만, 조회횟수가 높은 순으로 조회된다. - 테스트 코드 작성 (RedisTemplate, ZSetOperations Mocking)

--- backend/book-service/src/main/java/kr/mybrary/bookservice/booksearch/domain/BookSearchRankingService.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/booksearch/domain/BookSearchRankingService.java
@@ -1,8 +1,15 @@
 package kr.mybrary.bookservice.booksearch.domain;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import kr.mybrary.bookservice.booksearch.presentation.dto.response.BookSearchRankingResponse;
+import kr.mybrary.bookservice.booksearch.presentation.dto.response.BookSearchRankingResponse.BookSearchRanking;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +20,8 @@ public class BookSearchRankingService {
 
     private static final String RANKING_KEY = "bookSearchRanking";
     private static final int RANKING_INCREMENT_SCORE = 1;
+    private static final int RANKING_START = 0;
+    private static final int RANKING_END = 9;
 
     private final RedisTemplate<String, String> redisTemplate;
 
@@ -23,5 +32,18 @@ public class BookSearchRankingService {
         } catch (Exception e) {
             log.error("Fail Increase Search Ranking Score At {} Keyword, {}", keyword, e);
         }
+    }
+
+    public BookSearchRankingResponse getBookSearchKeywordRankingList() {
+        ZSetOperations<String, String> ZSetOperations = redisTemplate.opsForZSet();
+        Set<TypedTuple<String>> rankingSet = ZSetOperations.reverseRangeWithScores(RANKING_KEY, RANKING_START, RANKING_END);
+
+        List<BookSearchRanking> bookSearchKeywordRanking = Optional.ofNullable(rankingSet)
+                .map(keywords -> keywords.stream()
+                        .map(keyword -> BookSearchRanking.of(keyword.getValue(), keyword.getScore()))
+                        .toList())
+                .orElseGet(List::of);
+
+        return BookSearchRankingResponse.of(bookSearchKeywordRanking);
     }
 }
---
+++ backend/book-service/src/test/java/kr/mybrary/bookservice/booksearch/domain/BookSearchRankingServiceTest.java
@@ -0,0 +1,104 @@
+package kr.mybrary.bookservice.booksearch.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.verify;
+import static org.mockito.BDDMockito.when;
+import static org.mockito.Mockito.times;
+
+import java.util.HashSet;
+import java.util.Set;
+import kr.mybrary.bookservice.booksearch.presentation.dto.response.BookSearchRankingResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class BookSearchRankingServiceTest {
+
+    @InjectMocks
+    private BookSearchRankingService bookSearchRankingService;
+
+    @Mock
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Mock
+    private ZSetOperations<String, String> zSetOperations;
+
+    private static final String RANKING_KEY = "bookSearchRanking";
+    private static final String BOOK_SEARCH_KEYWORD = "test_keyword";
+
+    @DisplayName("검색 키워드 랭킹 점수 증가한다.")
+    @Test
+    public void increaseSearchRankingScore() {
+
+        // given
+        when(redisTemplate.opsForZSet()).thenReturn(zSetOperations);
+
+        // when
+        bookSearchRankingService.increaseSearchRankingScore(BOOK_SEARCH_KEYWORD);
+
+        // then
+        verify(zSetOperations).incrementScore(RANKING_KEY, BOOK_SEARCH_KEYWORD, 1.0);
+    }
+
+    @DisplayName("검색 키워드 랭킹 리스트를 조회한다.")
+    @Test
+    public void getBookSearchKeywordRankingList() {
+
+        // given
+        when(redisTemplate.opsForZSet()).thenReturn(zSetOperations);
+
+        Set<ZSetOperations.TypedTuple<String>> mockSet = new HashSet<>(Set.of(
+                new DefaultTypedTuple<>("keyword1", 1.0),
+                new DefaultTypedTuple<>("keyword2", 2.0),
+                new DefaultTypedTuple<>("keyword3", 3.0),
+                new DefaultTypedTuple<>("keyword4", 4.0),
+                new DefaultTypedTuple<>("keyword5", 5.0),
+                new DefaultTypedTuple<>("keyword6", 6.0)
+        ));
+
+        when(zSetOperations.reverseRangeWithScores(RANKING_KEY, 0, 9)).thenReturn(mockSet);
+
+        // when
+        BookSearchRankingResponse response = bookSearchRankingService.getBookSearchKeywordRankingList();
+
+        // then
+        assertAll(
+                () -> assertThat(response.getBooks().size()).isEqualTo(6),
+                () -> verify(zSetOperations, times(1)).reverseRangeWithScores(RANKING_KEY, 0, 9)
+        );
+    }
+
+    @DisplayName("검색 히스토리가 없을 경우, 검색 키워드 랭킹 리스트를 조회시, 빈 리스트를 반환한다.")
+    @Test
+    public void returnEmptyListWhenNotExistSearchHistory() {
+
+        // given
+        when(redisTemplate.opsForZSet()).thenReturn(zSetOperations);
+        when(zSetOperations.reverseRangeWithScores(RANKING_KEY, 0, 9))
+                .thenReturn(new HashSet<>());
+
+        // when
+        BookSearchRankingResponse response = bookSearchRankingService.getBookSearchKeywordRankingList();
+
+        // then
+        assertAll(
+                () -> assertThat(response.getBooks()).isEmpty(),
+                () -> verify(zSetOperations, times(1)).reverseRangeWithScores(RANKING_KEY, 0, 9)
+        );
+    }
+}
+
+
+
+
+
