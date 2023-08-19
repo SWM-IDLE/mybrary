4b3c017a0325ed6e1d05a1efea2f131a937aaed6
DonggyuJin <jdk2531@gmail.com>
2023-08-19 16:26:06 +0900

    [IDLE-211] refactor: jwt 관련 로직 수정

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -112,6 +112,8 @@ class Init {
 
     final accessToken = await secureStorage.read(key: accessTokenKey);
     final refreshToken = await secureStorage.read(key: refreshTokenKey);
+    print('accessToken: $accessToken');
+    print('refreshToken: $refreshToken');
 
     if (accessToken == null || refreshToken == null) {
       return const SignInScreen();
