ebf822ffa61d5b2abb0eaec5b3a4c719fcba73a3
DonggyuJin <jdk2531@gmail.com>
2023-08-19 16:26:06 +0900

    [IDLE-211] refactor: jwt 관련 로직 수정

--- frontend/lib/utils/dios/auth_dio.dart
+++ frontend/lib/utils/dios/auth_dio.dart
@@ -1,15 +1,14 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mybrary/data/network/api.dart';
 import 'package:mybrary/res/constants/config.dart';
-import 'package:mybrary/utils/dios/dio_service.dart';
 import 'package:mybrary/utils/logics/parse_utils.dart';
 
 Future<Dio> authDio(BuildContext context) async {
-  Dio dio = DioService().to();
+  var dio = Dio();
 
   const secureStorage = FlutterSecureStorage();
 
@@ -19,45 +18,20 @@ Future<Dio> authDio(BuildContext context) async {
     final accessToken = await secureStorage.read(key: accessTokenKey);
 
     final jwtPayload = parseJwt(accessToken!);
+    final int expiredTime = jwtPayload['exp'];
 
-    final int todayTimeStamp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
-    final int expiredTimeStamp = jwtPayload['exp'];
+    DateTime expiredDateTime =
+        DateTime.fromMillisecondsSinceEpoch(expiredTime * 1000);
+    final int expiredTimeStamp = expiredDateTime.millisecondsSinceEpoch;
+
+    DateTime todayDateTime = DateTime.now();
+    final int todayTimeStamp = todayDateTime.millisecondsSinceEpoch;
 
     if (expiredTimeStamp < todayTimeStamp) {
-      options.data = {expiredKey: true};
-      return handler.reject(
-        DioException(requestOptions: options),
-        true,
-      );
-    }
-
-    options.headers[accessTokenHeaderKey] = '$jwtHeaderBearer$accessToken';
-    return handler.next(options);
-  }, onError: (error, handler) async {
-    if (error.response?.statusCode == 401) {
-      log('ERROR: Access 토큰 만료에 대한 클라이언트 및 서버 에러가 발생했습니다.');
-
       final accessToken = await secureStorage.read(key: accessTokenKey);
       final refreshToken = await secureStorage.read(key: refreshTokenKey);
 
-      Dio refreshDio = DioService().to();
-
-      refreshDio.interceptors.clear();
-      refreshDio.interceptors
-          .add(InterceptorsWrapper(onError: (error, handler) async {
-        if (error.response?.statusCode == 401) {
-          log('ERROR: Refresh 토큰 만료에 대한 서버 에러가 발생했습니다.');
-          await secureStorage.deleteAll();
-
-          if (context.mounted) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/signin',
-              (Route<dynamic> route) => false,
-            );
-          }
-        }
-        return handler.next(error);
-      }));
+      var refreshDio = Dio();
 
       refreshDio.options.headers[accessTokenHeaderKey] =
           '$jwtHeaderBearer$accessToken';
@@ -75,22 +49,37 @@ Future<Dio> authDio(BuildContext context) async {
       await secureStorage.write(key: accessTokenKey, value: newAccessToken);
       await secureStorage.write(key: refreshTokenKey, value: newRefreshToken);
 
-      error.requestOptions.headers[accessTokenHeaderKey] =
-          '$jwtHeaderBearer$newAccessToken';
+      options.headers[accessTokenHeaderKey] = '$jwtHeaderBearer$newAccessToken';
 
       final clonedRequest = await dio.request(
-        error.requestOptions.path,
+        options.path,
         options: Options(
-          method: error.requestOptions.method,
-          headers: error.requestOptions.headers,
+          method: options.method,
+          headers: options.headers,
         ),
-        data: error.requestOptions.data,
-        queryParameters: error.requestOptions.queryParameters,
+        data: options.data,
+        queryParameters: options.queryParameters,
       );
 
-      return handler.resolve(clonedRequest);
+      return handler.resolve(clonedRequest, true);
     }
 
+    options.headers[accessTokenHeaderKey] = '$jwtHeaderBearer$accessToken';
+    return handler.next(options);
+  }, onResponse: (Response response, ResponseInterceptorHandler handler) {
+    return handler.next(response);
+  }, onError: (error, handler) async {
+    if (error.response?.statusCode == 400) {
+      log('ERROR: Refresh 토큰 만료에 대한 서버 에러가 발생했습니다.');
+      await secureStorage.deleteAll();
+
+      if (context.mounted) {
+        Navigator.of(context).pushNamedAndRemoveUntil(
+          '/signin',
+          (Route<dynamic> route) => false,
+        );
+      }
+    }
     return handler.next(error);
   }));
 
