4f40909e842b6b6842181e83c131ea6629257109
Seyoung Lee <0112leesy@naver.com>
2023-08-20 02:20:16 +0900

    [IDLE-205] feat: MultipartFileUtil 메서드 추가
      - 주어진 Date에 따라 yyyy-MM-dd_HH-mm-ss-SSS 형식의 Timestamp 파일명을 만들어주는 메서드 구현

--- backend/user-service/src/main/java/kr/mybrary/userservice/global/util/MultipartFileUtil.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/global/util/MultipartFileUtil.java
@@ -2,14 +2,22 @@ package kr.mybrary.userservice.global.util;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MultipartFileUtil {
 
-    public static String generateFilePath(String path, String fileName, MultipartFile multipartFile) {
-        return path + fileName + "." + getFileExtension(multipartFile);
+    public static String generateFilePath(String path, String fileName, String fileExtension) {
+        return path + fileName + "." + fileExtension;
     }
 
     public static String getFileExtension(MultipartFile multipartFile) {
         return multipartFile.getContentType().split("/")[1];
+    }
+
+    public static String generateTimestampFileName(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SSS");
+        return dateFormat.format(date);
     }
 
 }
