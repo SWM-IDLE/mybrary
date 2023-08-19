6dd0f70dc35bb669f2581961542d67369af7159d
Seyoung Lee <0112leesy@naver.com>
2023-08-20 04:03:41 +0900

    [IDLE-230] fix: api gateway 토큰 인증 실패 시 상태 코드 400 -> 401 수정

--- backend/apigateway-server/src/main/java/kr/mybrary/apigatewayserver/exception/AccessTokenNotFoundException.java
+++ backend/apigateway-server/src/main/java/kr/mybrary/apigatewayserver/exception/AccessTokenNotFoundException.java
@@ -2,7 +2,7 @@ package kr.mybrary.apigatewayserver.exception;
 
 public class AccessTokenNotFoundException extends ApplicationException {
 
-    private final static int STATUS = 400;
+    private final static int STATUS = 401;
     private final static String ERROR_CODE = "T-01";
     private final static String ERROR_MESSAGE = "액세스 토큰이 존재하지 않습니다. 로그인이 필요합니다.";
 
--- backend/apigateway-server/src/main/java/kr/mybrary/apigatewayserver/exception/InvalidTokenException.java
+++ backend/apigateway-server/src/main/java/kr/mybrary/apigatewayserver/exception/InvalidTokenException.java
@@ -2,7 +2,7 @@ package kr.mybrary.apigatewayserver.exception;
 
 public class InvalidTokenException extends ApplicationException {
 
-    private final static int STATUS = 400;
+    private final static int STATUS = 401;
     private final static String ERROR_CODE = "T-03";
     private final static String ERROR_MESSAGE = "유효하지 않은 토큰입니다. 기간이 만료되었거나, 변조된 토큰입니다.";
 
