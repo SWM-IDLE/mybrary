4775688beff3bf40a1d303c42377819df4dd2eb8
Seyoung Lee <0112leesy@naver.com>
2023-08-20 04:15:20 +0900

    [IDLE-230] fix: user service 토큰 재발급 실패 시 상태 코드 400 -> 401 수정
      - 리프레쉬 토큰 유효성 검사 시 예외 세분화

--- backend/user-service/src/main/java/kr/mybrary/userservice/authentication/domain/exception/RefreshTokenNotFoundException.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/authentication/domain/exception/RefreshTokenNotFoundException.java
@@ -4,7 +4,7 @@ import kr.mybrary.userservice.global.exception.ApplicationException;
 
 public class RefreshTokenNotFoundException extends ApplicationException {
 
-    private final static int STATUS = 400;
+    private final static int STATUS = 401;
     private final static String ERROR_CODE = "A-02";
     private final static String ERROR_MESSAGE = "리프레쉬 토큰이 존재하지 않습니다.";
 
