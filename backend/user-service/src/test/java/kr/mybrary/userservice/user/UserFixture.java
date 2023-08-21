29f98fdf2442c7ca0195f17a1b5a6d9684e05496
Seyoung Lee <0112leesy@naver.com>
2023-08-21 16:05:07 +0900

    [IDLE-205] test: User 테스트 데이터 수정
      - UserFixture에 thumbnailUrl 필드 추가
      - createMockMultipartFile 함수화

--- backend/user-service/src/test/java/kr/mybrary/userservice/user/UserFixture.java
+++ backend/user-service/src/test/java/kr/mybrary/userservice/user/UserFixture.java
@@ -14,28 +14,34 @@ public enum UserFixture {
 
     COMMON_USER(1L, "loginId", "nickname", "encodedPassword", Role.USER,
             "socialId", SocialType.GOOGLE, "email@mail.com", "introduction", "profileImageUrl",
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             Collections.emptyList(), Collections.emptyList()),
     
     USER_WITHOUT_PROFILE_IMAGE_URL(1L, "loginId", "nickname", "encodedPassword", Role.USER,
             "socialId", SocialType.GOOGLE, "email@mail.com", "introduction", null,
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             Collections.emptyList(), Collections.emptyList()),
 
     USER_WITHOUT_EMAIL(1L, "loginId", "nickname", "encodedPassword", Role.USER,
             "socialId", SocialType.GOOGLE, null, "introduction", "profileImageUrl",
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             Collections.emptyList(), Collections.emptyList()),
 
     USER_WITH_FOLLOWER(1L, "followingId", "nickname", "encodedPassword", Role.USER,
             "socialId", SocialType.GOOGLE, "email@mail.com", "introduction", "profileImageUrl",
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             List.of(Follow.builder().source(User.builder().loginId("followerId").build()).target(User.builder().loginId("followingId").build()).build()),
             Collections.emptyList()),
 
     USER_AS_FOLLOWER(1L, "followerId", "nickname", "encodedPassword", Role.USER,
             "socialId", SocialType.GOOGLE, "email@mail.com", "introduction", "profileImageUrl",
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             Collections.emptyList(),
             List.of(Follow.builder().source(User.builder().loginId("followerId").build()).target(User.builder().loginId("followingId").build()).build())),
 
     USER_WITH_SIMILAR_NICKNAME(2L, "loginId123", "nickname123", "encodedPassword", Role.USER,
             "socialId123", SocialType.GOOGLE, "email@mail.com", "introduction", "profileImageUrl",
+            "profileImageThumbnailTinyUrl", "profileImageThumbnailSmallUrl",
             Collections.emptyList(), Collections.emptyList());
 
     private final Long id;
@@ -48,6 +54,8 @@ public enum UserFixture {
     private final String email;
     private final String introduction;
     private final String profileImageUrl;
+    private final String profileImageThumbnailTinyUrl;
+    private final String profileImageThumbnailSmallUrl;
     private final List<Follow> followers;
     private final List<Follow> followings;
 
@@ -63,6 +71,8 @@ public enum UserFixture {
                 .email(email)
                 .introduction(introduction)
                 .profileImageUrl(profileImageUrl)
+                .profileImageThumbnailTinyUrl(profileImageThumbnailTinyUrl)
+                .profileImageThumbnailSmallUrl(profileImageThumbnailSmallUrl)
                 .followers(followers)
                 .followings(followings)
                 .build();
