bb455db3e9633cbf4c8f9083789f4e156c15a8df
DonggyuJin <jdk2531@gmail.com>
2023-08-22 02:54:06 +0900

    [IDLE-210] fix: refreshToken 재발급 관련 이슈 해결 - 연결 요청에 대한 timeout 시간 설정 - 다중 요청에 대해 Queued 인터셉터로 요청

--- frontend/lib/utils/dios/auth_dio.dart
+++ frontend/lib/utils/dios/auth_dio.dart
@@ -3,35 +3,57 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:mybrary/data/network/api.dart';
 import 'package:mybrary/res/constants/config.dart';
+import 'package:mybrary/utils/dios/dio_service.dart';
 
 Future<Dio> authDio(BuildContext context) async {
-  var dio = Dio();
+  var dio = DioService().to();
 
   const secureStorage = FlutterSecureStorage();
 
   dio.interceptors.clear();
-
-  dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) async {
+  dio.interceptors
+      .add(QueuedInterceptorsWrapper(onRequest: (options, handler) async {
     final accessToken = await secureStorage.read(key: accessTokenKey);
+    log("API 요청 경로: ${options.path}");
 
     options.headers[accessTokenHeaderKey] = '$jwtHeaderBearer$accessToken';
     return handler.next(options);
   }, onError: (error, handler) async {
+    log("ERROR: Server 에러 코드 <${error.response!.statusCode}>");
+    log("ERROR: Server 에러 메세지 <${error.response!.data.toString()}>");
+
     if (error.response?.statusCode == 401) {
-      log('ERROR: Access 토큰 만료에 대한 클라이언트 및 서버 에러가 발생했습니다.');
+      log('ERROR: Access 토큰 만료에 대한 서버 에러가 발생했습니다.');
 
       final accessToken = await secureStorage.read(key: accessTokenKey);
       final refreshToken = await secureStorage.read(key: refreshTokenKey);
 
       var refreshDio = Dio();
 
+      refreshDio.interceptors.clear();
+      refreshDio.interceptors
+          .add(InterceptorsWrapper(onError: (error, handler) async {
+        if (error.response?.statusCode == 401) {
+          log('ERROR: Refresh 토큰 만료에 대한 서버 에러가 발생했습니다.');
+          await secureStorage.deleteAll();
+
+          if (!context.mounted) return;
+          Navigator.of(context).pushNamedAndRemoveUntil(
+              '/signin', (Route<dynamic> route) => false);
+        }
+        return handler.next(error);
+      }));
+
       refreshDio.options.headers[accessTokenHeaderKey] =
           '$jwtHeaderBearer$accessToken';
       refreshDio.options.headers[refreshTokenHeaderKey] =
           '$jwtHeaderBearer$refreshToken';
 
-      final refreshResponse = await refreshDio.get(error.requestOptions.path);
+      final refreshResponse = await refreshDio.get(
+        getApi(API.getRefreshToken),
+      );
 
       final newAccessToken = refreshResponse.headers[accessTokenHeaderKey]![0];
       final newRefreshToken =
@@ -55,15 +77,6 @@ Future<Dio> authDio(BuildContext context) async {
 
       return handler.resolve(clonedRequest);
     }
-    if (error.response?.statusCode == 401) {
-      log('ERROR: Refresh 토큰 만료에 대한 서버 에러가 발생했습니다.');
-      await secureStorage.deleteAll();
-
-      if (!context.mounted) return;
-      Navigator.of(context)
-          .pushNamedAndRemoveUntil('/signin', (Route<dynamic> route) => false);
-    }
-    return handler.next(error);
   }));
 
   return dio;
--- frontend/lib/utils/dios/dio_service.dart
+++ frontend/lib/utils/dios/dio_service.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:mybrary/data/network/api.dart';
 
@@ -15,34 +13,11 @@ class DioService {
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       sendTimeout: const Duration(seconds: 10),
-      // headers: {},
     );
     _dio = Dio(options);
-    _dio.interceptors.add(DioInterceptor());
   }
 
   Dio to() {
     return _dio;
-  }
-}
-
-class DioInterceptor extends Interceptor {
-  @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    log("API 요청 경로: ${options.path}");
-    super.onRequest(options, handler);
-  }
-
-  @override
-  void onResponse(Response response, ResponseInterceptorHandler handler) {
-    log("${response.requestOptions.path} 의 응답 코드: ${response.statusCode}");
-    super.onResponse(response, handler);
-  }
-
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) async {
-    log("ERROR: Server 에러 <${err.error}>");
-    log("ERROR: Server 에러 메세지 <${err.message}>");
-    super.onError(err, handler);
   }
 }
