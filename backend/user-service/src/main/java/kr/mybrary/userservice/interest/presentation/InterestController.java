2efeea466115857ddcb35250d1804f6c5a0146b6
minseong <kdsvip5@naver.com>
2023-08-22 09:17:26 +0900

    [IDLE-229] feat: 사용자의 모든 관심사와 하나의 관심사의 도서 추천 목록을 조회 API Endpoint 구현 - GET "/interests/book-recommendations/{type}?page=1" - 하나의 관심사는 사용자의 모든 관심사 중 첫번째 관심사로 선택되어 해당 관심사에 대한 추천 도서 목록을 조회한다.

--- backend/user-service/src/main/java/kr/mybrary/userservice/interest/presentation/InterestController.java
+++ backend/user-service/src/main/java/kr/mybrary/userservice/interest/presentation/InterestController.java
@@ -2,12 +2,20 @@ package kr.mybrary.userservice.interest.presentation;
 
 import kr.mybrary.userservice.global.dto.response.SuccessResponse;
 import kr.mybrary.userservice.interest.domain.InterestService;
+import kr.mybrary.userservice.interest.domain.dto.request.UserInterestAndBookRecommendationsServiceRequest;
 import kr.mybrary.userservice.interest.domain.dto.request.UserInterestUpdateServiceRequest;
 import kr.mybrary.userservice.interest.presentation.dto.request.UserInterestUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,4 +51,17 @@ public class InterestController {
         );
     }
 
+    @GetMapping("/interests/book-recommendations/{type}")
+    public ResponseEntity<SuccessResponse> getInterestsAndBookRecommendations(@RequestHeader("USER-ID") String userId,
+                                                                              @PathVariable String type,
+                                                                              @RequestParam int page) {
+
+        UserInterestAndBookRecommendationsServiceRequest request =
+                UserInterestAndBookRecommendationsServiceRequest.of(userId, type, page);
+
+        return ResponseEntity.ok().body(
+                SuccessResponse.of(HttpStatus.OK.toString(), "사용자의 모든 관심사와 그 중 하나의 관심사에 대한 추천 도서를 조회했습니다.",
+                        interestService.getInterestsAndBookRecommendations(request))
+        );
+    }
 }
