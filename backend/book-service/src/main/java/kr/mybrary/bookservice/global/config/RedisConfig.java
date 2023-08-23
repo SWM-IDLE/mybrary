0c983a210292e9d0210e06777c9c5c44a956b9fa
minseong <kdsvip5@naver.com>
2023-08-23 17:20:05 +0900

    [IDLE-233] config: RedisConfig, RedisClusterConfig 분리 - RedisConfig는 local과 test 환경에서 사용 - RedisClusterConfig는 dev와 prod 환경에서 사용

--- backend/book-service/src/main/java/kr/mybrary/bookservice/global/config/RedisConfig.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/global/config/RedisConfig.java
@@ -1,14 +1,29 @@
 package kr.mybrary.bookservice.global.config;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import kr.mybrary.bookservice.global.redis.CacheKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
+@Profile({"local", "test"})
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -23,11 +38,35 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory cf) {
+
+        RedisCacheManagerBuilder builder = RedisCacheManagerBuilder.fromConnectionFactory(cf);
+
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofDays(1L))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        Map<String, RedisCacheConfiguration> cacheConfigurations = Arrays.stream(CacheKey.values())
+                .collect(Collectors.toMap(
+                        CacheKey::getKey,
+                        cacheKey -> RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofSeconds(cacheKey.getExpireTimeSeconds()))
+                                .prefixCacheNameWith(cacheKey.getPrefix() + "::")
+                ));
+
+        return builder.cacheDefaults(configuration).withInitialCacheConfigurations(cacheConfigurations).build();
     }
 }
