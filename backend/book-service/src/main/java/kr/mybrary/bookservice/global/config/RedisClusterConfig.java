dfa65a9ad3ce8de2453cd1c2dc911c62bf57d4b0
minseong <kdsvip5@naver.com>
2023-08-23 17:20:05 +0900

    [IDLE-233] config: RedisConfig, RedisClusterConfig 분리 - RedisConfig는 local과 test 환경에서 사용 - RedisClusterConfig는 dev와 prod 환경에서 사용

--- backend/book-service/src/main/java/kr/mybrary/bookservice/global/config/RedisClusterConfig.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/global/config/RedisClusterConfig.java
@@ -1,24 +1,52 @@
 package kr.mybrary.bookservice.global.config;
 
+
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import kr.mybrary.bookservice.global.redis.CacheKey;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager.RedisCacheManagerBuilder;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-public class RedisCacheConfig {
+@Profile({"prod", "dev"})
+public class RedisClusterConfig {
+
+    @Value("${spring.redis.cluster.nodes}")
+    private List<String> clusterNodes;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+        return new LettuceConnectionFactory(clusterConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory cf) {
@@ -41,4 +69,5 @@ public class RedisCacheConfig {
 
         return builder.cacheDefaults(configuration).withInitialCacheConfigurations(cacheConfigurations).build();
     }
+
 }
