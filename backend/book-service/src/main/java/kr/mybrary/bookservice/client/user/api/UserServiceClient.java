ba6b584544a96c8514d04a293bcd800115a45148
minseong <kdsvip5@naver.com>
2023-08-29 05:51:05 +0900

    [IDLE-228] feat: UserService FeignClient @Retry 적용 - feignClient 예외 중 임시적으로 발생할 수 있는 예외를 선택하여 retryExceptions에 등록 - retry 실패시 fallback 메서드를 통해 임시 userId와 default Profile Image Url를 반환하도록 구현 - interface에 feignClient 외에 fallback 함수와 같이 많은 코드가 포함되어 있어 분리가 필요

--- backend/book-service/src/main/java/kr/mybrary/bookservice/client/user/api/UserServiceClient.java
+++ backend/book-service/src/main/java/kr/mybrary/bookservice/client/user/api/UserServiceClient.java
@@ -1,8 +1,11 @@
 package kr.mybrary.bookservice.client.user.api;
 
 import feign.Headers;
+import io.github.resilience4j.retry.annotation.Retry;
 import kr.mybrary.bookservice.client.user.dto.request.UserInfoRequest;
 import kr.mybrary.bookservice.client.user.dto.response.UserInfoServiceResponse;
+import kr.mybrary.bookservice.client.user.dto.response.UserInfoServiceResponse.UserInfoList;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +13,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
 
+    String DEFAULT_PROFILE_IMAGE_URL = "https://mybrary-user-service-resized.s3.ap-northeast-2.amazonaws.com/tiny-profile/profileImage/default.jpg";
+    
     @PostMapping("/api/v1/users/info")
     @Headers("Content-Type: application/json")
+    @Retry(name = "userServiceRetryConfig", fallbackMethod = "getUsersInfoFallback")
     UserInfoServiceResponse getUsersInfo(@RequestBody UserInfoRequest userInfoRequest);
 
+    default UserInfoServiceResponse getUsersInfoFallback(UserInfoRequest userInfoRequest, Exception ex) {
+        return UserInfoServiceResponse.builder()
+                .data(makeTemporaryResponse(userInfoRequest))
+                .build();
+    }
+
+    private UserInfoList makeTemporaryResponse(UserInfoRequest userInfoRequest) {
+        return UserInfoList.builder()
+                .userInfoElements(userInfoRequest.getUserIds().stream()
+                        .map(userId -> UserInfoServiceResponse.UserInfo.builder()
+                                .userId(userId)
+                                .nickname(makeTemporaryNickname(userId))
+                                .profileImageUrl(DEFAULT_PROFILE_IMAGE_URL)
+                                .build()
+                        ).toList()
+        ).build();
+    }
+
+    @NotNull
+    private String makeTemporaryNickname(String userId) {
+        return "user_" + userId.substring(0, 5).toLowerCase();
+    }
 }
